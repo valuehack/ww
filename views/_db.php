8bceb39a3d214dd898d9b8c92bb9fe50984d5b46
bartholos <um@wertewirtschaft.org>
2015-05-05 15:16:19 +0200

    Update _db.php

--- views/_db.php
+++ views/_db.php
@@ -1,8 +1,5 @@
 <?php
 
-// @$con=mysql_connect("wertewirtschaft1.db.6152056.hostedresource.com","wertewirtschaft1","Werte333wirte") or die ("cannot connect to MySQL");
-// mysql_select_db("wertewirtschaft1");
-
     @$con=mysql_connect(DB_HOST,DB_USER,DB_PASS) or die ("cannot connect to MySQL");
     mysql_select_db(DB_NAME);
 
