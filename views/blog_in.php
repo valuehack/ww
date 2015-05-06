5863c39360676f06f5652f45695a4e6aa11a90f9
bartholos <um@wertewirtschaft.org>
2015-05-06 14:23:15 +0200

    Pagination Update

--- views/blog_in.php
+++ views/blog_in.php
@@ -50,43 +50,127 @@ if(isset($_GET['id']))
 
 else 
 {
-	$per_page = 3; //pagination script
-	$n = 0;
+	$tbl_name="blog";		//your table name
+	// How many adjacent pages should be shown on each side?
+	$adjacents = 3;
+	
+	/* 
+	   First get total number of rows in data table. 
+	   If you have a WHERE clause in your query, make sure you mirror it here.
+	*/
+	$query = "SELECT COUNT(*) as num FROM $tbl_name";
+	$total_pages = mysql_fetch_array(mysql_query($query));
+	$total_pages = $total_pages[num];
+	
+	/* Setup vars for query. */
+	$targetpage = "blog_in.php"; 	//your file name  (the name of this file)
+	$limit = 5; 								//how many items to show per page
+	$page = $_GET['page'];
+	if($page) 
+		$start = ($page - 1) * $limit; 			//first item to display on this page
+	else
+		$start = 0;								//if no page var is given, set start to 0
+	
+	/* Get data. */
 	$sql = "SELECT * from blog WHERE publ_date<=CURDATE() order by publ_date desc, id asc";
 	
 	$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
 	
-	//Start Pagination Script
-	$total_results = mysql_num_rows($result); 
-	$total_pages = ceil($total_results / $per_page);//total pages we going to have
 
-	//-------------if page is setcheck------------------//
-	if (isset($_GET['page'])) {
-    	$show_page = $_GET['page'];             //it will telles the current page
-   	 if ($show_page > 0 && $show_page <= $total_pages) {
-       	 $start = ($show_page - 1) * $per_page;
-       	 $end = $start + $per_page;
-  	  } else {
-      	  // error - show first set of results
-     	   $start = 0;              
-      	  $end = $per_page;
-  	  }
-	} else {
-   	 // if page isn't set, show first set of results
-   	 $start = 0;
-   	 $end = $per_page;
+		/* Setup page vars for display. */
+	if ($page == 0) $page = 1;					//if no page var is given, default to 1.
+	$prev = $page - 1;							//previous page is page - 1
+	$next = $page + 1;							//next page is page + 1
+	$lastpage = ceil($total_pages/$limit);		//lastpage is = total pages / items per page, rounded up.
+	$lpm1 = $lastpage - 1;						//last page minus 1
+	
+	/* 
+		Now we apply our rules and draw the pagination object. 
+		We're actually saving the code to a variable in case we want to draw it more than once.
+	*/
+	$pagination = "";
+	if($lastpage > 1)
+	{	
+		$pagination .= "<div class=\"pagination\">";
+		//previous button
+		if ($page > 1) 
+			$pagination.= "<a href=\"$targetpage?page=$prev\">� previous</a>";
+		else
+			$pagination.= "<span class=\"disabled\">� previous</span>";	
+		
+		//pages	
+		if ($lastpage < 7 + ($adjacents * 2))	//not enough pages to bother breaking it up
+		{	
+			for ($counter = 1; $counter <= $lastpage; $counter++)
+			{
+				if ($counter == $page)
+					$pagination.= "<span class=\"current\">$counter</span>";
+				else
+					$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+			}
+		}
+		elseif($lastpage > 5 + ($adjacents * 2))	//enough pages to hide some
+		{
+			//close to beginning; only hide later pages
+			if($page < 1 + ($adjacents * 2))		
+			{
+				for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+				}
+				$pagination.= "...";
+				$pagination.= "<a href=\"$targetpage?page=$lpm1\">$lpm1</a>";
+				$pagination.= "<a href=\"$targetpage?page=$lastpage\">$lastpage</a>";		
+			}
+			//in middle; hide some front and some back
+			elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+			{
+				$pagination.= "<a href=\"$targetpage?page=1\">1</a>";
+				$pagination.= "<a href=\"$targetpage?page=2\">2</a>";
+				$pagination.= "...";
+				for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+				}
+				$pagination.= "...";
+				$pagination.= "<a href=\"$targetpage?page=$lpm1\">$lpm1</a>";
+				$pagination.= "<a href=\"$targetpage?page=$lastpage\">$lastpage</a>";		
+			}
+			//close to end; only hide early pages
+			else
+			{
+				$pagination.= "<a href=\"$targetpage?page=1\">1</a>";
+				$pagination.= "<a href=\"$targetpage?page=2\">2</a>";
+				$pagination.= "...";
+				for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+				}
+			}
+		}
+		
+		//next button
+		if ($page < $counter - 1) 
+			$pagination.= "<a href=\"$targetpage?page=$next\">next �</a>";
+		else
+			$pagination.= "<span class=\"disabled\">next �</span>";
+		$pagination.= "</div>\n";		
 	}
-	// display pagination
-	$page = intval($_GET['page']);
+?>
 
-	$tpages=$total_pages;
-	if ($page <= 0)
-    	$page = 1;
-
-	//End Pagination
-
-	while($entry = mysql_fetch_array($result))
-	{
+	<?php
+		while($entry = mysql_fetch_array($result))
+		{
+	
 		$id = $entry[id];
 		$title = $entry[title];
 		$private = $entry[private_text];
@@ -102,19 +186,11 @@ else
 		else {
 			echo $private;
 			echo " <a href='?id=$id'>&rarr; Weiterlesen</a><hr>";
+	
 		}
-	$n++;
-	}
-	$reload = $_SERVER['PHP_SELF'] . "?tpages=" . $tpages;
-	if ($n == $per_page) {
-                    echo '<div class="pagination"><ul>';
-                    if ($total_pages > 1) {
-                        echo paginate($reload, $show_page, $total_pages);
-                    }
-					echo "</ul></div>";
-	}
-}
-?>
+	?>
+
+<?=$pagination?>
 
 </div>
 <?php include('_side_in.php'); ?>
