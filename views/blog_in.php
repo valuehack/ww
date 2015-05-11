a3ff616b0637107f773e783b4eb61316f09470ce
bartholos <um@wertewirtschaft.org>
2015-05-11 18:24:08 +0200

    blog update

--- views/blog_in.php
+++ views/blog_in.php
@@ -224,11 +224,12 @@ else
 		if (strlen($private) > 500) {
 			echo substr ($private, 0, 500);
 			echo " ... <a class='weitere' href='?id=$id'> Weiterlesen</a>";
+			echo "<p class='linie'><img src='gfx/linie.png' alt=''></p>";
 		}
 		else {
 			echo $private;
 			echo " <a class='weitere' href='?id=$id'> Weiterlesen</a>";
-	
+			echo "<p class='linie'><img src='gfx/linie.png' alt=''></p>";
 		}
 	}
 }
