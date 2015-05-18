ca304087fad682bbea664f3477731b3324b4d2d4
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-05-18 15:37:50 +0200

    one more

--- views/basket.php
+++ views/basket.php
@@ -104,13 +104,12 @@ if(isset($_POST['checkout'])) {
             $itemsExtraArray = mysql_fetch_array($items_extra_result);
 
             $sum = $quantity*$itemsExtraArray[event_price];
-            $download_link = downloadurl('http://test.wertewirtschaft.net/secdown/sec_files/'.$key.'.pdf,'.$key);
 
             echo "<tr><td>".$itemsExtraArray[id]."&nbsp</td>";
             echo "<td><i>".ucfirst($itemsExtraArray[type])."</i> ".$itemsExtraArray[title]." <i>".$itemsExtraArray[format]."</i></td>";
             echo "<td>&nbsp; &nbsp;".$quantity."</td>";
             echo '<td><a href="';
-            $download_link = downloadurl('http://test.wertewirtschaft.net/secdown/sec_files/'.$key.'.pdf,'.$key);
+            $download_link = downloadurl('http://test.wertewirtschaft.net/secdown/sec_files/'.$key.'.pdf\','.$key);
             echo '" onclick="updateReferer(this.href);">Download</a></td></tr>';
 
             ?>
