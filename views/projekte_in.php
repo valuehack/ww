4816e1c0250a66b10edd4b4e0471447e1f449d50
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-05-18 16:49:01 +0200

    projekte

--- views/projekte_in.php
+++ views/projekte_in.php
@@ -41,56 +41,151 @@ if(isset($_POST['add'])){
   $_SESSION['basket'][$add_id] = $add_quantity; 
 }
 
-if ($_SESSION['Mitgliedschaft'] > 1) { 
 
-echo "<div>";
-echo '<p>Wir bieten Ihnen durch unsere zahlreichen Angebote stets vollen Gegenwert f&uuml;r Ihre Unterst&uuml;tzung an. Eine wirkliche Unterst&uuml;tzung, die eine Ausweitung unserer T&auml;tigkeit erlaubt, wird es aber freilich erst, wenn Sie &ndash; so wie wir &ndash; einen gewissen Idealismus an den Tag legen und zumindest einen Teil Ihres Beitrages f&uuml;r Angebote widmen, die Ihnen nicht sofort, direkt und exklusiv zugute kommen, sondern Bausteine mit langfristiger Wirkung sind. In der Wertewirtschaft finden Sie eine professionelle, seri&ouml;se und realistische Alternative, als B&uuml;rger in den langfristigen Bestand, die Entwicklung und das Gedeihen Ihrer Gesellschaft zu investieren. Ohne dieses b&uuml;rgerliche Engagement bliebe es bei der ewigen Polarisierung von Markt und Staat, die meist zugunsten der Gewalt entschieden wird. Wir &uuml;berlassen Ihnen aber freilich Ausma&szlig; und Verwendung Ihres Beitrages &ndash; bitte w&auml;hlen Sie jene Projekte aus, die Ihnen sinnvoll erscheinen. Wenn Sie selbst einen gr&ouml;&szlig;eren Beitrag als en Restbetrag Ihres regelm&auml;&szlig;igen Guthabens investieren k&ouml;nnen und Projektvorschl&auml;ge haben, freuen wir uns &uuml;ber <a href="mailto:info@wertewirtschaft.org">Ihre Nachricht</a>. Ab einer Investition von 25.000&euro; k&ouml;nnen Sie Gesellschafter und damit Miteigent&uuml;mer unseres Unternehmens werden.</p>';
-echo "</div>";
+if ($id = $_GET["id"])
+{
+  $sql="SELECT * from termine WHERE id='$id'";
+  $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+  $entry = mysql_fetch_array($result);
+  $title=$entry[title];
+  $avail=$entry[spots]-$entry[spots_sold];
+  $text=$entry[text];
 
+  echo '<h3 style="font-style:none;">'.ucfirst($entry3[type])." ".$entry3[title].'</h3>';
 
-$sql = "SELECT * from termine WHERE `type` LIKE 'project' AND spots_sold < spots order by id asc";
-$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+  if ($entry[text]) echo "<p>$entry3[text]</p>";
 
-?>
-<br>
+  if ($_SESSION['Mitgliedschaft'] == 1) { 
+    ?>
+      <!-- Button trigger modal -->
+     <input type="button" value="Investieren" data-toggle="modal" data-target="#myModal"> 
 
-<table style="width:100%;border-collapse: collapse">
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h2 class="modal-title" id="myModalLabel">Investieren</h2>
+          </div>
+          <div class="modal-body">
+            Spendenformular
 
+            </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Schließen</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
-<?php
-
-  while($entry = mysql_fetch_array($result))
-  {
-    $event_id = $entry[id];
-    $available_spots = $entry[spots] - $entry[spots_sold];
-   ?>
-
-    <tr>
-        <td class="bottomline" style="width:60%"><i><?php echo $event_id."</i> <b>".$entry[title];?></b>
-        <td class="bottomline" style="width:40%">
-          <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <i>Credits: </i>
-            <input type="hidden" name="add" value="<?php echo $event_id ?>" />
-            <input type="number" name="quantity" style="width:50px;" min="1" max="<?php echo $available_spots;?>">
-            <input type="submit" value="Add to Basket"></form>
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2"><?php echo $entry[text]; ?></td>
-    </tr>  
-    <tr>
-        <td class="bottomline" colspan="2" style="text-align:right"><i><?php echo $entry[spots_sold]." von ".$entry[spots]." möglichen Credits zugewiesen"; ?></i></td>
-    </tr>      
-    <tr><td>&nbsp;</td><td></td></tr>
-    
     <?php
     }
+    else {
+      ?>
+      <i><?php echo $avail." von ".$entry[spots]." möglichen Credits zugewiesen"; ?></i><br>
+
+      <div><form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <i>Credits: </i>
+        <input type="hidden" name="add" value="<?php echo $id ?>" />
+        <input type="number" name="quantity" style="width:50px;" min="1" max="<?php echo $avail;?>">
+        <input type="submit" value="Add to Basket">
+      </form></div>
+    <?php
+    }
+
+}
+
+else {
+
+  if ($_SESSION['Mitgliedschaft'] == 1) { 
+    echo "<div>";
+    echo "<p>In der Wertewirtschaft finden Sie eine professionelle, seri&ouml;se und realistische Alternative, als B&uuml;rger in den langfristigen Bestand, die Entwicklung und das Gedeihen Ihrer Gesellschaft zu investieren. Ohne dieses b&uuml;rgerliche Engagement bliebe es bei der ewigen Polarisierung von Markt und Staat, die meist zugunsten der Gewalt entschieden wird. Wir &uuml;berlassen Ihnen aber freilich Ausma&szlig; und Verwendung Ihres Beitrages &ndash; bitte w&auml;hlen Sie jene Projekte aus, die Ihnen sinnvoll erscheinen. Je nach H&ouml;he Ihrer Investition profitieren Sie als Anerkennung Ihres Beitrages von den Angeboten der Wertewirtschaft.</p>";
+    echo "</div>";
+
+    $sql = "SELECT * from termine WHERE `type` LIKE 'project' AND spots_sold < spots order by id asc";
+    $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
     ?>
-</table>
+    <br>
+
+    <table style="width:100%;border-collapse: collapse">
+
+
+    <?php
+
+      while($entry = mysql_fetch_array($result))
+      {
+        $event_id = $entry[id];
+       ?>
+
+        <tr>
+            <td class="bottomline"><i><?php echo $event_id."</i> <b>".$entry[title];?></b>
+        </tr>
+        <tr>
+            <td><?php echo $entry[text]; ?></td>
+        </tr>      
+        <tr><td>&nbsp;</td><td></td></tr>
+        
+        <?php
+        }
+        ?>
+    </table>
+
+  <?php 
+  } 
+
+  else {
+
+  echo "<div>";
+  echo '<p>Wir bieten Ihnen durch unsere zahlreichen Angebote stets vollen Gegenwert f&uuml;r Ihre Unterst&uuml;tzung an. Eine wirkliche Unterst&uuml;tzung, die eine Ausweitung unserer T&auml;tigkeit erlaubt, wird es aber freilich erst, wenn Sie &ndash; so wie wir &ndash; einen gewissen Idealismus an den Tag legen und zumindest einen Teil Ihres Beitrages f&uuml;r Angebote widmen, die Ihnen nicht sofort, direkt und exklusiv zugute kommen, sondern Bausteine mit langfristiger Wirkung sind. In der Wertewirtschaft finden Sie eine professionelle, seri&ouml;se und realistische Alternative, als B&uuml;rger in den langfristigen Bestand, die Entwicklung und das Gedeihen Ihrer Gesellschaft zu investieren. Ohne dieses b&uuml;rgerliche Engagement bliebe es bei der ewigen Polarisierung von Markt und Staat, die meist zugunsten der Gewalt entschieden wird. Wir &uuml;berlassen Ihnen aber freilich Ausma&szlig; und Verwendung Ihres Beitrages &ndash; bitte w&auml;hlen Sie jene Projekte aus, die Ihnen sinnvoll erscheinen. Wenn Sie selbst einen gr&ouml;&szlig;eren Beitrag als en Restbetrag Ihres regelm&auml;&szlig;igen Guthabens investieren k&ouml;nnen und Projektvorschl&auml;ge haben, freuen wir uns &uuml;ber <a href="mailto:info@wertewirtschaft.org">Ihre Nachricht</a>. Ab einer Investition von 25.000&euro; k&ouml;nnen Sie Gesellschafter und damit Miteigent&uuml;mer unseres Unternehmens werden.</p>';
+  echo "</div>";
+
+
+  $sql = "SELECT * from termine WHERE `type` LIKE 'project' AND spots_sold < spots order by id asc";
+  $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+  ?>
+  <br>
+
+  <table style="width:100%;border-collapse: collapse">
+
+
+  <?php
+
+    while($entry = mysql_fetch_array($result))
+    {
+      $event_id = $entry[id];
+      $available_spots = $entry[spots] - $entry[spots_sold];
+     ?>
+
+      <tr>
+          <td class="bottomline" style="width:60%"><i><?php echo $event_id."</i> <b>".$entry[title];?></b>
+          <td class="bottomline" style="width:40%">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+              <i>Credits: </i>
+              <input type="hidden" name="add" value="<?php echo $event_id ?>" />
+              <input type="number" name="quantity" style="width:50px;" min="1" max="<?php echo $available_spots;?>">
+              <input type="submit" value="Add to Basket"></form>
+          </td>
+      </tr>
+      <tr>
+          <td colspan="2"><?php echo $entry[text]; ?></td>
+      </tr>  
+      <tr>
+          <td class="bottomline" colspan="2" style="text-align:right"><i><?php echo $entry[spots_sold]." von ".$entry[spots]." möglichen Credits zugewiesen"; ?></i></td>
+      </tr>      
+      <tr><td>&nbsp;</td><td></td></tr>
+      
+      <?php
+      }
+      ?>
+  </table>
 
 
 
 <?php 
+}
 } 
 ?>
 
