b4225cf6a80437f572844880eca1d1d99ebbd54c
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-05-19 10:56:03 +0200

    new structure

--- profil/index.php
+++ profil/index.php
@@ -1,23 +1,12 @@
 <?php
 
-/**
- * A simple PHP Login Script / ADVANCED VERSION
- * For more versions (one-file, minimal, framework-like) visit http://www.php-login.net
- *
- * @author Panique
- * @link http://www.php-login.net
- * @link https://github.com/panique/php-login-advanced/
- * @license http://opensource.org/licenses/MIT MIT License
- */
-
 // check for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    echo PHP_VERSION;
     exit('Sorry, this script does not run on a PHP version smaller than 5.3.7 !');
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once('../libraries/password_compatibility_library.php');
+    require_once('libraries/password_compatibility_library.php');
 }
 // include the config
 require_once('../config/config.php');
@@ -30,29 +19,19 @@ require_once('../libraries/PHPMailer.php');
 
 // load the login class
 require_once('../classes/Login.php');
-require_once('../classes/Registration.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
 $login = new Login();
-$registration = new Registration();
 
 // ... ask if we are logged in here:
-if ($login->isUserLoggedIn() == true) 
-{
+if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("../views/seminare_in.php");
-    // echo "wtf";
-    #include("views/header2.inc.php");
+    include("../views/edit.php");
 
-
-} 
-else {
+} else {
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
-    include("../views/seminare_not_in.php");
-    // echo "hello";
-    #include("views/header2.inc.php");
-    
+    include("../views/index_not_in.php");
 }
