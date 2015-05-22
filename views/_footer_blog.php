b7360ce771b588ab7ee7ac5796cef8838424f09c
bartholos <um@wertewirtschaft.org>
2015-05-22 14:52:39 +0200

    footer update

--- views/_footer_blog.php
+++ views/_footer_blog.php
@@ -1,12 +1,12 @@
 <!--Footer-->
        <footer class="footer">&nbsp;
 		<p>
-	&copy; Institut f&uuml;r Wertewirtschaft&trade; - <a class="impressum" href="http://wertewirtschaft.org/institut/tradition.php">Schule</a> - <a class="impressum" href="http://wertewirtschaft.org/institut/zunft.php">Zunft</a> - <a class="impressum" href="http://wertewirtschaft.org/institut/ort.php">Ort</a> - <a class="impressum" href="http://wertewirtschaft.org/institut/ort.php#Kontakt">Impressum</a> - <a class="impressum" href="http://wertewirtschaft.org/institut/agb.php">AGB</a> - <a class="impressum" href="http://wertewirtschaft.org/institut/mitglied.php">Unterst&uuml;tzen</a>
-        </p>
+			<a class="impressum" href="http://wertewirtschaft.org/">&copy; Institut f&uuml;r Wertewirtschaft&trade;</a> &nbsp; 
+			<a class="impressum" href="http://wertewirtschaft.org/kontakt/">Impressum</a> &nbsp; 
+			<a class="impressum" href="http://wertewirtschaft.org/agb/p">AGB</a> &nbsp; 
+			<a class="impressum" href="http://wertewirtschaft.org/akteure/">Akteur</a>
        </footer>
-<!-- Layout -->
+<!-- End Layout -->
 
-
-</body>
-
+	</body>
 </html>
