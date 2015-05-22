9bad1f2185bedce779e68ab710dd2a05debb0006
bartholos <um@wertewirtschaft.org>
2015-05-22 15:53:05 +0200

    footer update

--- views/_footer_blog.php
+++ views/_footer_blog.php
@@ -4,6 +4,7 @@
 			<a href="http://wertewirtschaft.org/">&copy; Institut f&uuml;r Wertewirtschaft&trade;</a> &nbsp; 
 			<a href="http://wertewirtschaft.org/kontakt/">Impressum</a> &nbsp; 
 			<a href="http://wertewirtschaft.org/agb/">AGB</a> &nbsp; 
+			<a href="http://wertewirtschaft.org/akteure/">Akteure</a>
        </footer>
 <!-- End Layout -->
 
