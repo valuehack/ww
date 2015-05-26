cc9a657ce147bd5b7b3395b8842f9763fb835f8b
bartholos <um@wertewirtschaft.org>
2015-05-26 10:56:04 +0200

    header

--- views/_header.php
+++ views/_header.php
@@ -6,7 +6,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
     <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <!--<link rel="stylesheet" type="text/css" href="../style/style.css">-->
+    <link rel="stylesheet" type="text/css" href="../scholien/blog_style.css">
     <title><?=$title?> | Institut f&uuml;r Wertewirtschaft</title>
 
 <!--Dateien - Bildgalerie -->
