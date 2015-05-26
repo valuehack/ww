bf16982dd7add3765f2cd4f8dbe6be01a2d1fab1
bartholos <um@wertewirtschaft.org>
2015-05-26 15:19:03 +0200

    blog, header update

---
+++ buerger.php
@@ -0,0 +1,14 @@
+<?php 
+//Author: Bernhard Hegyi
+
+require_once('../classes/Login.php');
+include('_header_not_in.php'); 
+$title="B&uuml;rger | Wertewirtschaft";
+
+?>
+
+<div class="content">
+	
+</div>
+
+<?php include('_footer.php'); ?>
--- views/_header_not_in.php
+++ views/_header_not_in.php
@@ -1,63 +1,31 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
-  <head>
-<!-- <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> -->    
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!DOCTYPE html>
+<html lang="de">
+	<head>  
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title><?=$title?> | Institut f&uuml;r Wertewirtschaft</title>
+    
+    	<link rel="shortcut icon" href="/favicon.ico">
+    	<link rel="stylesheet" type="text/css" href="../style/style.css">
 
-    <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="../scholien/blog_style.css">
+    	<!-- this is used for this fancy login form -->
+    	<script language="javascript" src="/js/jquery.js"></script>
+    	<script language="javascript" src="/js/script.js"></script>
 
-    <title><?=$title?> | Institut f&uuml;r Wertewirtschaft</title>
+		<!-- Google Analytics Code -->
+		<script type="text/javascript">
+  			var _gaq = _gaq || [];
+  			_gaq.push(['_setAccount', 'UA-39285642-1']);
+  			_gaq.push(['_trackPageview']);
 
-<!--Dateien - Bildgalerie -->
-    <link rel="stylesheet" href="http://wertewirtschaft.org/tools/Lightbox/css/lightbox.css" type="text/css" media="screen" />
-
-    <script src="http://wertewirtschaft.org/tools/Lightbox/prototype.js" type="text/javascript"></script>
-    <script src="http://wertewirtschaft.org/tools/Lightbox/scriptaculous.js?load=effects,builder" type="text/javascript"></script>
-    <script src="http://wertewirtschaft.org/tools/Lightbox/lightbox.js" type="text/javascript"></script>
-<!-- Ende -->
-
-    <!-- this is used for this fancy login form -->
-    <SCRIPT LANGUAGE="JavaScript" src="/js/jquery.js"></SCRIPT>
-    <SCRIPT LANGUAGE="JavaScript" src="/js/script.js"></SCRIPT>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-39285642-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
-  </head>
-
-<script type="text/javascript">
-// Javascript originally by Patrick Griffiths and Dan Webb.
-// http://htmldog.com/articles/suckerfish/dropdowns/
-sfHover = function() {
-   var sfEls = document.getElementById("navbar").getElementsByTagName("li");
-   for (var i=0; i<sfEls.length; i++) {
-      sfEls[i].onmouseover=function() {
-         this.className+=" hover";
-      }
-      sfEls[i].onmouseout=function() {
-         this.className=this.className.replace(new RegExp(" hover\\b"), "");
-      }
-   }
-}
-if (window.attachEvent) window.attachEvent("onload", sfHover);
-</script>
-
-<body>
-<!-- Layout-->
-        <header class="header1">
+  			(function() {
+    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  			})();
+		</script>
+	</head>
+	<body>
+        <header class="header">
             <div class="logo">
                 <a href="/"><img class="logo_img" src="../scholien/gfx/ww_logo_w.png" alt="Institut f&uuml; Wertewirtschaft" name="Home"></a>
                 <span class="login"><a href="">Ulrich M&ouml;ller</a></span>
@@ -107,13 +75,3 @@ if (isset($registration)) {
     }
 }
 ?>
-
-<!-- test login -->
-<form method="post" action="index.php" name="loginform" style="text-aligna:center;">
-        <label for="user_email"><?php# echo WORDING_LOGIN; ?></label>
-        <input class="inputfield" id="user_email" type="text" name="user_email" placeholder=" E-Mail Adresse" required />
-        <label for="user_password"><?php #echo WORDING_PASSWORD; ?></label>
-        <input class="inputfield" id="user_password" type="password" name="user_password" placeholder=" Passwort" autocomplete="off" required />
-        <input class="inputbutton" type="submit" name="login" value="Anmelden" />
-    </form>
-
