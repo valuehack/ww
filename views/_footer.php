b9e4fc0bdeeb22a7e235419ba0c562f8510feb60
bartholos <um@wertewirtschaft.org>
2015-05-26 16:03:16 +0200

    footer

--- views/_footer.php
+++ views/_footer.php
@@ -1,5 +1,5 @@
         	<footer class="footer">
-				<p class="footer">
+				<p>
 					<a class="footer" href="http://wertewirtschaft.org/">&copy; Wertewirtschaft&trade;</a> &nbsp; 
 					<a class="footer" href="http://wertewirtschaft.org/kontakt/">Impressum</a> &nbsp; 
 					<a class="footer" href="http://wertewirtschaft.org/agb/">AGB</a> &nbsp; 
