44f819694f01ee489f7a0327b602856f46d9521c
bartholos <um@wertewirtschaft.org>
2015-05-28 15:29:53 +0200

    kurse_in

--- views/kurse_in.php
+++ views/kurse_in.php
@@ -46,11 +46,44 @@ if(isset($_GET['q']))
   $n = $entry3[n];
   
 ?>
-  	<div class="salon">
+  	<div class="salon_head">
  	 	<h1><?=ucfirst($entry3[type])." ".$entry3[title]?></h1>
   		<p class="salon_date"><?echo strftime("%d.%m.%Y %H:%M Uhr", strtotime($entry3[start]));?></p>
-		<img src="<?echo $img_url;?>" alt="<? echo $id;?>">
+		<!--<img src="<?echo $img_url;?>" alt="<? echo $id;?>">-->
 		
+		<div class="centered">
+			<div class="salon_reservation">
+				<?	
+  if ($_SESSION['Mitgliedschaft'] == 1) {
+    echo '<input class="salon_reservation_inputbutton" type="button" value="Anmelden" data-toggle="modal" data-target="#myModal1">';
+    }
+
+  if ($_SESSION['Mitgliedschaft'] == 2) {
+    echo '<input class="salon_reservation_inputbutton" type="button" value="Upgrade" data-toggle="modal" data-target="#myModal2">';  
+    } 
+  
+  if ($_SESSION['Mitgliedschaft'] > 2) {
+    ?>
+	<span class="salon_reservation_span_a">Anzahl gew&uuml;nschter Teilnehmer</span><br>
+    <form class="salon_reservation_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <input type="hidden" name="add" value="<?php echo $n; ?>" />      
+      <select name="quantity">
+      	<option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>        
+      </select> 
+      <input class="inputbutton" type="submit" value="Auswählen"><br>     
+    </form>
+	<span class="salon_reservation_span_b"><?php echo $entry3[price]; ?> Credits pro Teilnehmer</span>
+  <?php
+  }
+?>
+			</div>
+		</div>		
+	</div>
+	<div class="salon_content">
   <? 
   /* weekdays don't work
     $day=date("w",strtotime($entry3[start]));
@@ -60,43 +93,19 @@ if(isset($_GET['q']))
     
   if ($entry3[text]) echo "<p>$entry3[text]</p>";
   if ($entry3[text2]) echo "<p>$entry3[text2]</p>";
-
-	echo "<div class='salon_info'>";
-  echo "<p>Unsere Kurse inmitten unserer einzigartigen Bibliothek bieten inhaltliche Vertiefungen abseits des Mainstream-Lehrbetriebs. Wir folgen dabei dem Beispiel der klassischen Akademie &ndash; der Bibliothek im Hain der Mu&szlig;e fern vom Wahnsinn der Zeit, in der Freundschaften durch regen Austausch und gemeinsames Nachdenken gestiftet werden. Alle unsere Lehrangebote zeichnen sich durch geb&uuml;hrende Tiefe bei gleichzeitiger Verst&auml;ndlichkeit, kleine Gruppen und gro&szlig;en Freiraum f&uuml;r Fragen und Diskussionen aus. Tauchen Sie mit uns in intellektuelle Abenteuer, wie sie unsere Zeit kaum noch zul&auml;&szlig;t.</p>";
-	echo "</div>";
-  if ($_SESSION['Mitgliedschaft'] == 1) {
-    echo "Platzhalter Anmeldeformular";
-    }
-
-  if ($_SESSION['Mitgliedschaft'] == 2) {
-    echo "Platzhalter Upgrade";  
-    } 
-  
-  if ($_SESSION['Mitgliedschaft'] > 2) {
-    ?>
-    <form class="salon_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-      <input type="hidden" name="add" value="<?php echo $n; ?>" />
-      Menge: <select name="quantity">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>        
-      </select>
-      <input type="submit" value="Reservieren">&nbsp;<i><?php echo $entry3[price]; ?> Credits pro Reservierung</i>
-    </form>
-  <?php
-  }
+?>
+		<p>Unsere Kurse inmitten unserer einzigartigen Bibliothek bieten inhaltliche Vertiefungen abseits des Mainstream-Lehrbetriebs. Wir folgen dabei dem Beispiel der klassischen Akademie &ndash; der Bibliothek im Hain der Mu&szlig;e fern vom Wahnsinn der Zeit, in der Freundschaften durch regen Austausch und gemeinsames Nachdenken gestiftet werden. Alle unsere Lehrangebote zeichnen sich durch geb&uuml;hrende Tiefe bei gleichzeitiger Verst&auml;ndlichkeit, kleine Gruppen und gro&szlig;en Freiraum f&uuml;r Fragen und Diskussionen aus. Tauchen Sie mit uns in intellektuelle Abenteuer, wie sie unsere Zeit kaum noch zul&auml;&szlig;t.</p>
+	</div>
+	
+<?php
 }
-
-else {
-  
+  else { 
   if ($_SESSION['Mitgliedschaft'] == 1 || $_SESSION['Mitgliedschaft'] == 2) {
   ?>
     <div class="salon_info">    
     	<p>Unsere Kurse inmitten unserer einzigartigen Bibliothek bieten inhaltliche Vertiefungen abseits des Mainstream-Lehrbetriebs. Wir folgen dabei dem Beispiel der klassischen Akademie &ndash; der Bibliothek im Hain der Mu&szlig;e fern vom Wahnsinn der Zeit, in der Freundschaften durch regen Austausch und gemeinsames Nachdenken gestiftet werden. Alle unsere Lehrangebote zeichnen sich durch geb&uuml;hrende Tiefe bei gleichzeitiger Verst&auml;ndlichkeit, kleine Gruppen und gro&szlig;en Freiraum f&uuml;r Fragen und Diskussionen aus. Tauchen Sie mit uns in intellektuelle Abenteuer, wie sie unsere Zeit kaum noch zul&auml;&szlig;t.</p>
     </div> 
- 		<div class="salon">
+ 		<div class="salon_content">
   <?
   } 
   
@@ -109,9 +118,9 @@ else {
   {
     $found=1;
     $id = $entry[id];
-    echo "<h3>";
+    echo "<h1>";
     echo "<a href='?q=$id'>";
-    echo ucfirst($entry[type])." ".$entry[title]."</a></h3>";
+    echo ucfirst($entry[type])." ".$entry[title]."</a></h1>";
      
     echo "<div class='salon_date'>";
     /* weekdays don't work:
@@ -122,21 +131,39 @@ else {
     echo date("d.m.Y",strtotime($entry[start]));
     echo "</div>";
     echo "<p>";
-    echo "<img src='$img_url' alt='$id'>";
+    //echo "<img src='$img_url' alt='$id'>";
     echo $entry[text];
     echo "<div class='salon_anmeldung'><a href='?q=$id'>zur Anmeldung</a></div>";
 	echo "<div class='centered'><p class='linie'><img src='../style/gfx/linie.png' alt=''></p></div>";
-
+	echo "</div>";
   } 
 } 
 
 ?>
 
 	</div>
+
+<!-- Modal 1 - Mitgliedschaft == 1 -->
+<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h2 class="modal-title" id="myModalLabel">Mitgliedschaft 150</h2>
+      </div>
+      <div class="modal-body">
+        Erklärung 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Schließen</button>
+        <a href="../upgrade.php"><button type="button" class="btn btn-primary">Jetzt upgraden</button></a>
+      </div>
+    </div>
+  </div>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<!-- Modal 2 - Mitgliedschaft == 2 -->
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
