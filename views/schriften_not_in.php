a65e8e5a6a100478ed1ce35ec39b4512340747e2
bartholos <um@wertewirtschaft.org>
2015-06-01 17:31:29 +0200

    schriften

--- views/schriften_not_in.php
+++ views/schriften_not_in.php
@@ -91,15 +91,15 @@ while($entry = mysql_fetch_array($result))
 	
 ?>
 		<tr>
-			<td>	
-				<!--<? echo ucfirst($entry[type]);?>
-			</td>-->
-			<td>
-      			<? echo "<a href='?q=$id'>".$entry[title]." </a>"; ?><br>
-      			<? echo ucfirst($entry[type]);?>
-			</td>
 			<td>
 				<img src="<?echo $img_url;?>" alt="Cover <?echo $id;?>">
+			</td>			
+			<td>
+				<span><? echo ucfirst($entry[type]);?></span>
+      			<? echo "<a href='?q=$id'>".$entry[title]." </a>"; ?><br>
+			</td>
+			<td>	
+				<input type="button" class="inputbutton" value="Bestellen und Herunterladen" data-toggle="modal" data-target="#myModal">
 			</td>
 		</tr>
 
