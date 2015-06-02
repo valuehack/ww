7b1624ae3b3c6967f79c867375b788cbb8c0a881
Rahim <rt@wertewirtschaft.org>
2015-06-02 11:48:38 +0200

    Abo-not in

--- abo/index.php
+++ abo/index.php
@@ -34,5 +34,5 @@ if ($login->isUserLoggedIn() == true) {
 } else {
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
-    include("../views/not_logged_in.php");
+    include("../views/index_not_in.php");
 }
