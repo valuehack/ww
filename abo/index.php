3eab483bdf3ba4c41a44159f1653f249ca37dd9b
Rahim <rt@wertewirtschaft.org>
2015-06-02 08:51:00 +0200

    Abo-Ansicht

--- abo/index.php
+++ abo/index.php
@@ -29,7 +29,7 @@ if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
     #echo "this works";
-    include("../views/upgradeView.php");
+    include("../views/upgrade_in.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
