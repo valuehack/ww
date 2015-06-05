9a298cb99a1d9674287d5822b7847bc7c095fa93
bartholos <um@wertewirtschaft.org>
2015-06-05 17:42:18 +0200

    Impressum

---
+++ kontakt/index.php
@@ -0,0 +1,38 @@
+<? 
+require_once('../classes/Login.php');
+include ("../views/_header_not_in.php");
+$title="AGB";
+?>
+
+<div class="content">
+	<div class="agb">
+		<h1>Impressum</h1>
+		
+		<h2>Disclaimer</h2>
+			<p>Disclaimer</p>
+			
+		<h2>Kontakt</h2>
+		<div class="location_box">  
+   		<table>
+   			<tr>
+   				<td>
+   					<h1>Wertewirtschaft</h1>
+   					<ul>
+    					<li>Schl&ouml;sselgasse 19/2/18</li>
+    					<li>A 1080 Wien</li>
+    					<li>&Ouml;sterreich</li>
+    					<li>&nbsp;</li>
+    					<li>Fax: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+43 1 2533033 4733</li>
+    					<li>E-Mail: &nbsp;<a href="mailto:&#105;nf&#111;&#064;&#119;&#101;rt&#101;wirtsc&#104;&#097;f&#116;.or&#103;">&#105;nf&#111;&#064;&#119;&#101;rt&#101;wirtsc&#104;&#097;f&#116;.or&#103;</a></li>
+   					</ul>
+   				</td> 
+   				<td>
+   					<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.de/maps?f=q&amp;source=s_q&amp;hl=de&amp;geocode=&amp;q=Schl%C3%B6sselgasse+19%2F18+1080+Wien,+%C3%96sterreich&amp;aq=0&amp;oq=Schl%C3%B6sselgasse+19%2F18,+1080+Wien&amp;sll=51.175806,10.454119&amp;sspn=7.082438,21.643066&amp;ie=UTF8&amp;hq=&amp;hnear=Schl%C3%B6sselgasse+19,+Josefstadt+1080+Wien,+%C3%96sterreich&amp;t=m&amp;z=14&amp;ll=48.213954,16.353095&amp;output=embed"></iframe><br /><small><a href="https://maps.google.de/maps?f=q&amp;source=embed&amp;hl=de&amp;geocode=&amp;q=Schl%C3%B6sselgasse+19%2F18+1080+Wien,+%C3%96sterreich&amp;aq=0&amp;oq=Schl%C3%B6sselgasse+19%2F18,+1080+Wien&amp;sll=51.175806,10.454119&amp;sspn=7.082438,21.643066&amp;ie=UTF8&amp;hq=&amp;hnear=Schl%C3%B6sselgasse+19,+Josefstadt+1080+Wien,+%C3%96sterreich&amp;t=m&amp;z=14&amp;ll=48.213954,16.353095"></iframe>
+   				</td>
+   			</tr>
+   		</table>
+   </div>
+	</div>
+</div>
+
+<? include "../views/_footer.php"; ?>
