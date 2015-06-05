e5df0a97c67161fb448458b7bd775a742f01d524
bartholos <um@wertewirtschaft.org>
2015-06-05 17:20:21 +0200

    Bibliothek

--- views/bibliothek_in.php
+++ views/bibliothek_in.php
@@ -1,27 +1,23 @@
 <?php 
-//Author: Bernhard Hegyi
-
 require_once('../classes/Login.php');
 include('_header_in.php'); 
 $title="Bibliothek";
 
 ?>
 
-<div class="medien_info">
-		<h1>Bibliothek</h1>
+<div class="content"></div>
+		<div class="medien_info">
+			<h1>Bibliothek</h1>
 		
-		
-
-<p>Die Wertewirtschaft verf&uuml;gt &uuml;ber die wohl bedeutendste Bibliothek zu den Themenbereichen realistischer &Ouml;konomik, Unternehmertum, Freiheit und praktischer Philosophie &ndash; dank zahlreicher Stiftungen, insbesondere derjenigen des Privatgelehrten Roland Baader (Kirrlach), des Saloniers und Freidenkers Rainer Ernst Sch&uuml;tz (Wien), des &Ouml;konomen Hans-Hermann Hoppe (Istanbul) und des Wirtschaftsphilosophen Rahim Taghizadegan (Wien). Diese Bibliothek ist f&uuml;r unsere G&auml;ste zug&auml;nglich und nutzbar. Wir sind noch immer mit der gro&szlig;en Arbeit der vollst&auml;ndigen Erfassung und des digitalen Zugangs besch&auml;ftigt.</p>
+			<p>Die Wertewirtschaft verf&uuml;gt &uuml;ber die wohl bedeutendste Bibliothek zu den Themenbereichen realistischer &Ouml;konomik, Unternehmertum, Freiheit und praktischer Philosophie &ndash; dank zahlreicher Stiftungen, insbesondere derjenigen des Privatgelehrten Roland Baader (Kirrlach), des Saloniers und Freidenkers Rainer Ernst Sch&uuml;tz (Wien), des &Ouml;konomen Hans-Hermann Hoppe (Istanbul) und des Wirtschaftsphilosophen Rahim Taghizadegan (Wien). Diese Bibliothek ist f&uuml;r unsere G&auml;ste zug&auml;nglich und nutzbar. Wir sind noch immer mit der gro&szlig;en Arbeit der vollst&auml;ndigen Erfassung und des digitalen Zugangs besch&auml;ftigt.</p>
 
 <?php
 if ($_SESSION['Mitgliedschaft'] == 1) {
-
+	
 	echo '<p>Wir freuen uns, dass Sie unsere Bibliothek nutzen m&ouml;chten. Allerdings steht diese nur unseren G&auml;sten zur Verf&uuml;gung, die einen kleinen Kostenbeitrag (6,25&euro;) f&uuml;r das Bestehen der Wertewirtschaft leisten (und daf&uuml;r B&uuml;cher auch kostenlos entleihen k&ouml;nnen). K&ouml;nnen Sie sich das leisten? Dann folgen Sie <a href="http://test.wertewirtschaft.net/abo/">diesem Link</a>. Noch ist die Bibliothek nur vor Ort nutzbar, bzw. sind Anfragen zur Fernleihe per E-Mail m&ouml;glich; wir arbeiten daran, <a href="http://test.wertewirtschaft.net/abo/">G&auml;sten</a> bald einen digitalen Zugriff zu erlauben.</p>';
 
 }
 ?>
-
-
+	</div>
 </div>
 <?php include('_footer.php'); ?>
--- views/bibliothek_not_in.php
+++ views/bibliothek_not_in.php
@@ -10,13 +10,13 @@ $title="Bibliothek";
 
   			<p>Die Wertewirtschaft verf&uuml;gt &uuml;ber die wohl bedeutendste Bibliothek zu den Themenbereichen realistischer &Ouml;konomik, Unternehmertum, Freiheit und praktischer Philosophie &ndash; dank zahlreicher Stiftungen, insbesondere derjenigen des Privatgelehrten Roland Baader (Kirrlach), des Saloniers und Freidenkers Rainer Ernst Sch&uuml;tz (Wien), des &Ouml;konomen Hans-Hermann Hoppe (Istanbul) und des Wirtschaftsphilosophen Rahim Taghizadegan (Wien). Diese Bibliothek ist f&uuml;r unsere G&auml;ste zug&auml;nglich und nutzbar. Wir sind noch immer mit der gro&szlig;en Arbeit der vollst&auml;ndigen Erfassung und des digitalen Zugangs besch&auml;ftigt.</p>
 			<p>Wenn Sie ein Zugang interessiert, hinterlassen Sie uns Ihre E-Mail-Adresse:</p>
-		</div>
   
-  		<div class="subscribe">
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="registerform">
-        		<input class="inputfield" type="email" placeholder=" E-Mail Adresse" name="user_email" autocomplete="off" required>
-        		<input class="inputbutton" id="inputbutton" type="submit" name="submit" value="Eintragen">
-      		</form>	
+  			<div class="subscribe">
+				<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="registerform">
+        			<input class="inputfield" type="email" placeholder=" E-Mail Adresse" name="user_email" autocomplete="off" required>
+        			<input class="inputbutton" id="inputbutton" type="submit" name="submit" value="Eintragen">
+      			</form>	
+			</div>
 		</div>
 </div>
 
