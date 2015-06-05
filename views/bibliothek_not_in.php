b15ea3a7fd25e31a4400f391d60916d473a6f773
bartholos <um@wertewirtschaft.org>
2015-06-05 17:16:06 +0200

    Fixed Biblitohek -> Xhtml to html5, css statt <br> etc

--- views/bibliothek_not_in.php
+++ views/bibliothek_not_in.php
@@ -1,28 +1,23 @@
 <?php 
-//Author: Bernhard Hegyi
-
 require_once('../classes/Login.php');
 include('_header_not_in.php'); 
 $title="Bibliothek";
 
 ?>
+<div class="content">
+		<div class="medien_info">
+			<h1>Bibliothek</h1>
 
-<div class="medien_info">
-		<h1>Bibliothek</h1>
-
-  		<p>Die Wertewirtschaft verf&uuml;gt &uuml;ber die wohl bedeutendste Bibliothek zu den Themenbereichen realistischer &Ouml;konomik, Unternehmertum, Freiheit und praktischer Philosophie &ndash; dank zahlreicher Stiftungen, insbesondere derjenigen des Privatgelehrten Roland Baader (Kirrlach), des Saloniers und Freidenkers Rainer Ernst Sch&uuml;tz (Wien), des &Ouml;konomen Hans-Hermann Hoppe (Istanbul) und des Wirtschaftsphilosophen Rahim Taghizadegan (Wien). Diese Bibliothek ist f&uuml;r unsere G&auml;ste zug&auml;nglich und nutzbar. Wir sind noch immer mit der gro&szlig;en Arbeit der vollst&auml;ndigen Erfassung und des digitalen Zugangs besch&auml;ftigt.</p>
-		<p>Wenn Sie ein Zugang interessiert, hinterlassen Sie uns Ihre E-Mail-Adresse:</p>
-	</div>
-  
-  <div class="subscribe">
-		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="registerform">
-        <input class="inputfield" id="keyword" type="email" placeholder=" E-Mail Adresse" name="user_email" autocomplete="off" required />
-        <input class="inputfield" id="user_password" type="password" name="user_password" placeholder=" Passwort" autocomplete="off" style="display:none"  />
-        <input class="inputbutton" id="inputbutton" type="submit" name="fancy_ajax_form_submit" value="Eintragen" />
-      </form>	
+  			<p>Die Wertewirtschaft verf&uuml;gt &uuml;ber die wohl bedeutendste Bibliothek zu den Themenbereichen realistischer &Ouml;konomik, Unternehmertum, Freiheit und praktischer Philosophie &ndash; dank zahlreicher Stiftungen, insbesondere derjenigen des Privatgelehrten Roland Baader (Kirrlach), des Saloniers und Freidenkers Rainer Ernst Sch&uuml;tz (Wien), des &Ouml;konomen Hans-Hermann Hoppe (Istanbul) und des Wirtschaftsphilosophen Rahim Taghizadegan (Wien). Diese Bibliothek ist f&uuml;r unsere G&auml;ste zug&auml;nglich und nutzbar. Wir sind noch immer mit der gro&szlig;en Arbeit der vollst&auml;ndigen Erfassung und des digitalen Zugangs besch&auml;ftigt.</p>
+			<p>Wenn Sie ein Zugang interessiert, hinterlassen Sie uns Ihre E-Mail-Adresse:</p>
 		</div>
-		<br /><br />
-		<p class="linie"><img src="../style/gfx/linie.png" alt=""></p>
-		<br /><br /><br /><br /><br /><br /><br /><br />
+  
+  		<div class="subscribe">
+			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="registerform">
+        		<input class="inputfield" type="email" placeholder=" E-Mail Adresse" name="user_email" autocomplete="off" required>
+        		<input class="inputbutton" id="inputbutton" type="submit" name="submit" value="Eintragen">
+      		</form>	
+		</div>
+</div>
 
 <? include "_footer.php"; ?>
