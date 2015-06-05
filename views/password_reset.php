bcfaa3c1c4fb45206bb683e5daedbe055981e6d2
bartholos <um@wertewirtschaft.org>
2015-06-05 18:41:11 +0200

    Password

--- views/password_reset.php
+++ views/password_reset.php
@@ -1,18 +1,18 @@
 <?php include('_header.php'); ?>
 
 <div class="content">
-	<div class="agb">
+	<div class="password">
 <?php if ($login->passwordResetLinkIsValid() == true) { ?>
 
 <form class="passwort" method="post" action="/" name="new_password_form">
     <input type='hidden' name='user_email' value='<?php echo $_GET['user_email']; ?>' />
     <input type='hidden' name='user_password_reset_hash' value='<?php echo $_GET['verification_code']; ?>' />
 <br>
-    <label for="user_password_new">New password</label>
+    <label class="passwort_label" for="user_password_new">Neues Passwort</label>
     <br>
     <input id="user_password_new" class="inputfield" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
 <br>
-    <label for="user_password_repeat"><?php echo WORDING_NEW_PASSWORD_REPEAT; ?></label>
+    <label class="passwort_label" for="user_password_repeat"><?php echo WORDING_NEW_PASSWORD_REPEAT; ?></label>
     <br>
     <input id="user_password_repeat" class="inputfield" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
 <br>
@@ -22,8 +22,8 @@
 <!-- no data from a password-reset-mail has been provided, so we simply show the request-a-password-reset form -->
 <?php } else { ?>
 		<h2>Passwort zur&uuml;cksetzen</h2>
-		<form class="passwort" method="post" action="/password_reset.php" name="password_reset_form">
-    		<label for="user_email"><?php echo WORDING_REQUEST_PASSWORD_RESET; ?></label>
+		<form  method="post" action="/password_reset.php" name="password_reset_form">
+    		<label  for="user_email"><?php echo WORDING_REQUEST_PASSWORD_RESET; ?></label><br>
     		<input id="user_email" class="inputfield" type="text" name="user_email" required />
     		<input type="submit" class="inputbutton" name="request_password_reset" value="<?php echo WORDING_RESET_PASSWORD; ?>" />
 		</form>
