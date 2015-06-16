700b986052f2e67fad8e7a566e0920468ca2cafc
bartholos <um@wertewirtschaft.org>
2015-06-16 13:06:06 +0200

    Startpage

--- views/index_not_in.php
+++ views/index_not_in.php
@@ -20,39 +20,103 @@ include ("_header_not_in.php");
 
 <!--Content-->
 
-    <div class="content">
-    	
-	  		<div class="startpage_section_a">
-                <div class="startpage_info">
-                    <p>Die Wertewirtschaft ist ein lernendes Unternehmen, in dem Wege werte- und sinnorientierten Unternehmertums praktisch erkundet und theoretisch reflektiert werden. Wir bieten eine Orientierungshilfe f&uuml;r kritische B&uuml;rger und eine Bildungsalternative f&uuml;r junge Menschen, die der heutigen Blasenwirtschaft, aber auch ideologischen Versprechen misstrauen.</p>
-                </div>
-                <div class="startpage_box black">
+        <div class="content">
+            
+            <div class="startpage_section_a">
+                <div class="startpage_box_events">
+                    <h1>Aktuelle Veranstaltungen</h1>
+                    <h2>Salons</h2>
+                        <?php
+$sql = "SELECT * from produkte WHERE (type LIKE 'salon') AND status > 0 order by id asc, n asc LIMIT 0, 3";
+$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+while($entry = mysql_fetch_array($result))
+{
+                $id = $entry[id]; 
+				echo "<a href='/salon/index.php?q=$id'>";
+				echo date("d.m.Y",strtotime($entry[start]));
+              	if (strtotime($entry[end])>(strtotime($entry[start])+86400)) echo "-".date("d.m.Y",strtotime($entry[end]));
+              	echo ": $entry[title]</a><br>";                  
+}
+                    ?>
+                    <h2>Kurse</h2>
+                        <?php
+$sql = "SELECT * from produkte WHERE (type LIKE 'kurs') AND status > 0 order by id asc, n asc LIMIT 0, 3";
+$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+while($entry = mysql_fetch_array($result))
+{
+                $id = $entry[id];
+				echo "<a href='/kurse/index.php?q=$id'>";
+				echo date("d.m.Y",strtotime($entry[start]));
+              	if (strtotime($entry[end])>(strtotime($entry[start])+86400)) echo "-".date("d.m.Y",strtotime($entry[end]));
+              	echo ": $entry[title]</a><br>";                  
+}
+                    ?>
+                </div>  
+                
+                <div class="startpage_box_scholien black">
                     <h1>Neue Scholien</h1>
+                    <?php
+$sql = "SELECT * from blog WHERE publ_date<=CURDATE() order by publ_date desc, id asc LIMIT 0, 2";
+$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+while($entry = mysql_fetch_array($result))
+{
+                $id = $entry[id]; 
+                echo "<a href='/scholien/index.php?q=$id'><i>".date('d.m.Y', strtotime($publ_date))."</i> ".$entry[title]."</a><br>";                  
+}
+                    ?>
                 </div>
             </div>
             
             <div class="startpage_section_b">
-            	<h1>Neue Inhalte</h1>
-                <div class="startpage_box white">
+                <h1>Neue Inhalte</h1>
+                <div class="startpage_box_contents white">
                     <h1>Schriften</h1>
+                     <?php
+$sql = "SELECT * from produkte WHERE (type LIKE 'buch' OR type LIKE 'scholie' OR type LIKE 'analyse') AND status > 0 order by id asc, n asc LIMIT 0, 3";
+$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+while($entry = mysql_fetch_array($result))
+{
+                $id = $entry[id]; 
+                echo "<a href='/schriften/index.php?q=$id'>".$entry[title]." <i>(".ucfirst($entry[type]).")</i></a><br>";                  
+}
+                    ?>
                 </div>
-                <div class="startpage_box white">
+                <div class="startpage_box_contents white startpage_box_contents_middle">
                     <h1>Medien</h1>
+                    <?php
+$sql = "SELECT * from produkte WHERE (type LIKE 'media' OR type LIKE 'audio' OR type LIKE 'video') AND status > 0 order by id asc, n asc LIMIT 0, 3";
+$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+while($entry = mysql_fetch_array($result))
+{
+                $id = $entry[id]; 
+                echo "<a href='/medien/index.php?q=$id'>".$entry[title]." <i>(".ucfirst($entry[type]).")</i></a><br>";                  
+}
+                    ?>
                 </div>
-                <div class="startpage_box white">
-                    <h1>Projekte</h1>
+                <div class="startpage_box_contents white">
+                    <h1>Letzte Spende</h1>
+                    <?php
+$sql = "SELECT * from produkte WHERE (type LIKE 'projekt') AND status > 0 order by id asc, n asc LIMIT 0, 3";
+$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+
+while($entry = mysql_fetch_array($result))
+{
+                $id = $entry[id]; 
+                echo "<a href='/projekte/index.php?q=$id'>".$entry[title]."</a><br>";                  
+}
+                    ?>
                 </div>
-            </div>
-            
+            </div>  
             <div class="startpage_section_c">
-            	<h1>Aktuelle Veranstaltungen</h1>
-                <div class="startpage_box black">
-                    <h1>Salons</h1>
-                </div>       
-                <div class="startpage_box black">
-                    <h1>Kurse</h1>
-                </div>         
-            </div>
+            	 <div class="startpage_info">
+                    <p>Die Wertewirtschaft ist ein lernendes Unternehmen, in dem Wege werte- und sinnorientierten Unternehmertums praktisch erkundet und theoretisch reflektiert werden. Wir bieten eine Orientierungshilfe f&uuml;r kritische B&uuml;rger und eine Bildungsalternative f&uuml;r junge Menschen, die der heutigen Blasenwirtschaft, aber auch ideologischen Versprechen misstrauen.</p>
+                </div>
+            </div>            
      
      <?php 
      #include ("views/sidebar.inc.php"); 
