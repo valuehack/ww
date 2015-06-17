6ed6d52fe23209f15104db0ab39ce7e74535ef37
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-06-17 12:51:33 +0200

    bürger

--- eltern.php
+++ eltern.php
@@ -6,20 +6,20 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once('../libraries/password_compatibility_library.php');
+    require_once('libraries/password_compatibility_library.php');
 }
 // include the config
-require_once('../config/config.php');
+require_once('config/config.php');
 
 // include the to-be-used language, english by default. feel free to translate your project and include something else
-require_once('../translations/en.php');
+require_once('translations/en.php');
 
 // include the PHPMailer library
-require_once('../libraries/PHPMailer.php');
+require_once('libraries/PHPMailer.php');
 
 // load the login class
-require_once('../classes/Login.php');
-require_once('../classes/Registration.php');
+require_once('classes/Login.php');
+require_once('classes/Registration.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
@@ -30,11 +30,11 @@ $title="Eltern";
 // ... ask if we are logged in here:
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
-    include("../views/_header_in.php");
+    include("views/_header_in.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
-    include("../views/_header_not_in.php");
+    include("views/_header_not_in.php");
     
 }
 
