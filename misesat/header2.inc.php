845988417e9b0dd5983a969722cc6ed6314ec955
bartholos <um@wertewirtschaft.org>
2015-06-17 13:35:35 +0200

    Misesat Header

--- misesat/header2.inc.php
+++ misesat/header2.inc.php
@@ -3,7 +3,7 @@
 	
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-    <link rel="shortcut icon" href="http://wienerschule.org/favicon.ico">
+    <link rel="shortcut icon" href="http://test.wertewirtschaft.net/misesat/favicon.ico">
     <!--[if IE]>
       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
     <![endif]-->
@@ -15,24 +15,20 @@
 <body>
 <!--Header-->
   <header class="header">
-  	<h1>Wiener &nbsp;&nbsp;&nbsp; Schule</h1>
-  	<p>&Ouml;sterreichische Schule</p>
+  	<div class="logo">
+  		<h1>Mises Austria</h1>
+	</div>
+  	<div class="nav">
+    	<div class="navi">
+      		<ul>
+        		<li><a href="#">Begriffe</a></li>
+        		<li><a href="autoren/index.php">Autoren</a></li>
+        		<li><a href="orte/index.php">Orte</a></li>
+        		<li><a href="#">Dinge</a></li>
+        		<li><a href="#">Dinge</a></li>
+      		</ul>
+   		</div>
+  	</div>
   </header>
-  <!--Main Nav-->
-  <div class="nav_main_outer">
-  	<div class="nav_main_inner">
-    <div class="navi">
-      <ul>
-        <li><a href="#">Begriffe</a></li>
-        <li><a href="autoren/index.php">Autoren</a></li>
-        <li><a href="orte/index.php">Orte</a></li>
-        <li><a href="#">Dinge</a></li>
-        <li><a href="#">Dinge</a></li>
-      </ul>
-    </div>
-    <div class="nav_main_quote">
-    	<blockquote>"Ein freier Mensch muss es ertragen k&ouml;nnen, da&szlig; seine Mitmenschen anders handeln und anders leben, als er es f&uuml;r richtig h&auml;lt, und mu&szlig; sich abgew&ouml;hnen, sobald ihm etwas nicht gef&auml;llt, nach der Polizei zu rufen."</blockquote><p>Ludwig von Mises</p>
-    </div>
-    </div>
-  </div>
+  
 <!--End Main Nav-->
