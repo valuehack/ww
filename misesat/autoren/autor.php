c388796c95c8b68dc585d7865c483a177c8605d9
bartholos <um@wertewirtschaft.org>
2015-06-17 17:12:20 +0200

    Misesat Denker

--- misesat/autoren/autor.php
+++ misesat/autoren/autor.php
@@ -8,63 +8,63 @@
 
 <!--Content-->
 
-    <article class="content">
+    <div class="content">
+      <article class="denker">
+      	<div class="index"><p><a href="">Wiener Schule</a> / <a href="">Denker</a> / <a href="">Ludwig von Mises</a></p></div>
       
-      <p class="index"><a class="index" href="">Wiener Schule</a> / <a class="index" href="">Denker</a> / <a class="index" href="">Ludwig von Mises</a></p>
+      	<h1>Ludwig Edler von Mises</h1>
       
-      <h2>Ludwig Edler von Mises</h2>
+      	<img src="mises.jpg" class="autor" alt="">
       
-      <img src="mises.jpg" class="autor" alt="">
+      	<section>
+      	<h2>Leben</h2>
       
-      <h3>Leben</h3>
+      	<p>Ludwig Edler von Mises wurde am 29. September 1881 als Sohn des Arthur Edlen von Mises und seiner Frau Adele, geb. Landau, als Sohn einer wohlhabenden j&uuml;dischen Familie in Lemberg (damals Hauptstadt des Kronlandes Galizien, heute Lwiw, Ukraine) geboren. Die Erhebung in den erblichen Adelsstand war f&uuml;r seinen Urgro�vater Mayer Rachmiel Mises durch Kaiser Franz Joseph I. erfolgt. Sein Bruder war der Mathematiker Richard von Mises. Wenige Jahre sp&auml;ter siedelte die Familie nach Wien &uuml;ber, wo Mises 1900 sein Studium der Rechtswissenschaft aufnahm und 1906 mit einer Dissertation abschloss. Von Mises war ab 1906 Mitarbeiter der Handels- und Gewerbekammer in Wien und leitete dort die Finanzabteilung. Er lehrte ab 1913 in einer unbezahlten Privatdozentur, ab 1918 als au�erordentlicher Professor an der Universit&auml;t Wien sowie ab 1934 am Institut universitaire de hautes �tudes internationales in Genf. W&auml;hrend seiner Zeit in Wien hielt er zudem Privatseminare in seinem B&uuml;ro bei der Handelskammer ab, Teilnehmer waren etwa Fritz Machlup, Oskar Morgenstern und Eric Voegelin. Auch das heutige Wirtschaftsforschungsinstitut (WIFO) geht auf seine Gr&uuml;ndung zur&uuml;ck.[1] Von Mises war einer der f&uuml;hrenden Wirtschaftsberater der &ouml;sterreichischen Regierung; sein wichtigster Mitarbeiter zu dieser Zeit war Friedrich August von Hayek.</p>
       
-      <p>Ludwig Edler von Mises wurde am 29. September 1881 als Sohn des Arthur Edlen von Mises und seiner Frau Adele, geb. Landau, als Sohn einer wohlhabenden j&uuml;dischen Familie in Lemberg (damals Hauptstadt des Kronlandes Galizien, heute Lwiw, Ukraine) geboren. Die Erhebung in den erblichen Adelsstand war f&uuml;r seinen Urgro�vater Mayer Rachmiel Mises durch Kaiser Franz Joseph I. erfolgt. Sein Bruder war der Mathematiker Richard von Mises. Wenige Jahre sp&auml;ter siedelte die Familie nach Wien &uuml;ber, wo Mises 1900 sein Studium der Rechtswissenschaft aufnahm und 1906 mit einer Dissertation abschloss. Von Mises war ab 1906 Mitarbeiter der Handels- und Gewerbekammer in Wien und leitete dort die Finanzabteilung. Er lehrte ab 1913 in einer unbezahlten Privatdozentur, ab 1918 als au�erordentlicher Professor an der Universit&auml;t Wien sowie ab 1934 am Institut universitaire de hautes �tudes internationales in Genf. W&auml;hrend seiner Zeit in Wien hielt er zudem Privatseminare in seinem B&uuml;ro bei der Handelskammer ab, Teilnehmer waren etwa Fritz Machlup, Oskar Morgenstern und Eric Voegelin. Auch das heutige Wirtschaftsforschungsinstitut (WIFO) geht auf seine Gr&uuml;ndung zur&uuml;ck.[1] Von Mises war einer der f&uuml;hrenden Wirtschaftsberater der &ouml;sterreichischen Regierung; sein wichtigster Mitarbeiter zu dieser Zeit war Friedrich August von Hayek.</p>
-      
-      <div class="booksource">
-      	<p>
-      		<img src="wienerschule_buch.jpg" title="Die Wiener Schule der National&oouml;konomie von Eugen-Maria Schulak und Hebert Unterk&ouml;fler" alt="">
+      	<div class="booksource">
+      		<p>
+      			<img src="wienerschule_buch.jpg" title="Die Wiener Schule der National&oouml;konomie von Eugen-Maria Schulak und Hebert Unterk&ouml;fler" alt="">
       		Die hier vorgestellte Biografie enstammt dem Buch <i>&quot;Die Wiener Schule der National&ouml;konomie&quot;</i>, geschrieben von <a href="#">Eugen-Maria Schulak</a> und Hebert Unterk&ouml;fler. Das Werk erschien in der Reihe &quot;Wiener Wissen&quot; und ist die bislang am tiefsten gehende Darstellung der historischen Entwicklung der Wiener Schule der &Ouml;konomie. Umfassende Recherchen bef&ouml;rderten viel Unbekanntes und &Uuml;berraschendes zu Tage. Neben Biografien und der historischen Entwicklung werden auch die Ideen der Wiener Schule leicht verst&auml;ndlich dargestellt.<br><br>
       		
       		<b>&quot;Die Wiener Schule der National&ouml;konomie&quot;</b> beim <a href="#">Institut f&uuml;r Wertewirtschaft</a> und bei <a href="#">Amazon.de</a>.
 
-      	</p>
-      </div>
+      		</p>
+      	</div>
       
-      <p>Aus der Schweiz emigrierte von Mises im Jahr 1940 in die USA, weil er sich in Europa zunehmend bedroht f&uuml;hlte. In den USA hatte er als konsequenter Liberaler in der Phase des New Deal beruflich zun&auml;chst einen schweren Stand und mu�te von Ersparnissen leben. 1946 erhielt er die US-amerikanische Staatsb&uuml;rgerschaft verliehen. Er unterrichtete von 1945 bis 1969 ? damals als &auml;ltester lehrender Professor in den USA ? an der New York University an einem Stiftungslehrstuhl. Von Mises war Mitglied der liberalen Denkfabrik Mont Pelerin Society.</p>
-      
-     <section class="works">
+      	<p>Aus der Schweiz emigrierte von Mises im Jahr 1940 in die USA, weil er sich in Europa zunehmend bedroht f&uuml;hlte. In den USA hatte er als konsequenter Liberaler in der Phase des New Deal beruflich zun&auml;chst einen schweren Stand und mu�te von Ersparnissen leben. 1946 erhielt er die US-amerikanische Staatsb&uuml;rgerschaft verliehen. Er unterrichtete von 1945 bis 1969 ? damals als &auml;ltester lehrender Professor in den USA ? an der New York University an einem Stiftungslehrstuhl. Von Mises war Mitglied der liberalen Denkfabrik Mont Pelerin Society.</p>
+      	</section>
+     	<section class="works">
      	
-     	<h3>Werke</h3>
+     		<h2>Werke</h2>
      	
-     	<span class="works_sub">
-          <h4>B&uuml;cher</h4>
-     
-            <ul>
-              <li><a href="">Theorie des Geldes und der Umlaufsmittel</a></li>
-              <li><a href="">Die Gemeinwirtschaft</a></li>
-              <li><a href="">Human Action - A Treatise in Economics</a></li>
-           </ul>
-       </span>
+     		<span class="works_sub">
+          		<h4>B&uuml;cher</h4>
+            	<ul>
+              		<li><a href="">Theorie des Geldes und der Umlaufsmittel</a></li>
+              		<li><a href="">Die Gemeinwirtschaft</a></li>
+              		<li><a href="">Human Action - A Treatise in Economics</a></li>
+          		</ul>
+       		</span>
         
-        <span class="works_sub_m">
-          <h4>Artikel</h4>
-     
-            <ul>
-              <li><a href="">Soziologie und Geschichte</a></li>
-              <li><a href="">Begreifen und Verstehen</a></li>
-            </ul>
-        </span>
+        	<span class="works_sub_m">
+          		<h4>Artikel</h4>
+            	<ul>
+              		<li><a href="">Soziologie und Geschichte</a></li>
+              		<li><a href="">Begreifen und Verstehen</a></li>
+            	</ul>
+       		</span>
         
-        <span class="works_sub">
-          <h4>Biografien</h4>
+        	<span class="works_sub">
+          		<h4>Biografien</h4>
      
-            <ul>
-              <li><a href="">The Last Knight of Liberalism</a></li>
-              <li><a href="">Ludwig von Mises: Der Mensch und sein Werk</a></li>
-            </ul>
-        </span>
+           		<ul>
+              		<li><a href="">The Last Knight of Liberalism</a></li>
+              		<li><a href="">Ludwig von Mises: Der Mensch und sein Werk</a></li>
+            	</ul>
+        	</span>
         <div class="clear"></div>
-    </article>
+       </div>
+    </div>
 
 <?	
 	include "../footer.inc.php";
