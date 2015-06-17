11f35b0b6f7df45a16038a56964860af06070521
bartholos <um@wertewirtschaft.org>
2015-06-17 12:13:50 +0200

    misesat

---
+++ misesat/artikel/artikel.php
@@ -0,0 +1,19 @@
+<? 
+	include "../header1.inc.php";
+
+	$title="Beispielartikel";
+
+	include "../header2.inc.php";
+?>
+
+<!--Content-->
+
+    <div id="content">
+     <a class="index" href="../index.php">Home &raquo;</a> <a class="index" href="index.php">Artikel &raquo;</a> <a class="index" href="#">Beispielartikel</a>
+     <div id="wrapper_lower"><h2>Beispielartikel</h2></div> 
+    </div>
+
+<?	
+	include "../sidebar.inc.php";
+	include "../footer.inc.php";
+?>
