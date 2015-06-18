7436ebc89f232e942b8ebb7f31fe78c529c57c8f
bartholos <um@wertewirtschaft.org>
2015-06-18 16:35:31 +0200

    misesat_autor

--- misesat/autoren/index.php
+++ misesat/autoren/index.php
@@ -1,14 +1,86 @@
 <? 
 	include "../config/header1.inc.php";
 
-	$title="Autoren";
+	$title="Denker";
 
 	include "../page/header2.inc.php";
-?>
+	
+if(isset($_GET['q']))
+{
+  $id = $_GET['q'];
 
+  //Autorendetails
+  $sql="SELECT * from denker WHERE id='$id'";
+  $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+  $entry = mysql_fetch_array($result);
+  $name = $entry[name];
+  $bio = $entry[bio];
+  $img = $entry[img];
+?>
+<!--Denker-->
 <!--Content-->
 
-  <article class="content">
+    <div class="content">
+      <article class="denker">
+      	<div class="index"><p><a href="">Wiener Schule</a> / <a href="">Denker</a> / <a href=""><?=$name?></a></p></div>
+      
+      	<h1><?=$name?></h1>
+      	     
+      	<section>
+      	<h2>Leben</h2>
+      
+      	<img src="<?=$img?>" class="denker" alt="Portr&auml;t von <?=$name?>">
+      	
+      	<p><?=$bio?></p>
+      
+      	<div class="booksource">
+      		<p>
+      			<img src="wienerschule_buch.jpg" title="Die Wiener Schule der National&oouml;konomie von Eugen-Maria Schulak und Hebert Unterk&ouml;fler" alt="">
+      		Die hier vorgestellte Biografie enstammt dem Buch <i>&quot;Die Wiener Schule der National&ouml;konomie&quot;</i>, geschrieben von <a href="#">Eugen-Maria Schulak</a> und Hebert Unterk&ouml;fler. Das Werk erschien in der Reihe &quot;Wiener Wissen&quot; und ist die bislang am tiefsten gehende Darstellung der historischen Entwicklung der Wiener Schule der &Ouml;konomie. Umfassende Recherchen bef&ouml;rderten viel Unbekanntes und &Uuml;berraschendes zu Tage. Neben Biografien und der historischen Entwicklung werden auch die Ideen der Wiener Schule leicht verst&auml;ndlich dargestellt.<br><br>
+      		
+      		<b>&quot;Die Wiener Schule der National&ouml;konomie&quot;</b> beim <a href="#">Institut f&uuml;r Wertewirtschaft</a> und bei <a href="#">Amazon.de</a>.
+
+      		</p>
+      	</div>
+      	</section>
+     	<section class="works">
+     	
+     		<h1>Werke</h1>
+     	
+     		<span class="works_sub">
+          		<h4>B&uuml;cher</h4>
+            	<ul>
+              		<li><a href="">Theorie des Geldes und der Umlaufsmittel</a></li>
+              		<li><a href="">Die Gemeinwirtschaft</a></li>
+              		<li><a href="">Human Action - A Treatise in Economics</a></li>
+          		</ul>
+       		</span>
+        
+        	<span class="works_sub_m">
+          		<h4>Artikel</h4>
+            	<ul>
+              		<li><a href="">Soziologie und Geschichte</a></li>
+              		<li><a href="">Begreifen und Verstehen</a></li>
+            	</ul>
+       		</span>
+        
+        	<span class="works_sub">
+          		<h4>Biografien</h4>
+     
+           		<ul>
+              		<li><a href="">The Last Knight of Liberalism</a></li>
+              		<li><a href="">Ludwig von Mises: Der Mensch und sein Werk</a></li>
+            	</ul>
+        	</span>
+        <div class="clear"></div>
+       </div>
+    </div>
+<?php
+}
+else {
+?>
+<!--Denkerliste-->	
+<article class="content">
   	
   	  <p class="index"><a class="index" href="">Wiener Schule</a> / <a class="index" href="">Denker</a></p>
       
@@ -219,7 +291,7 @@
       </section>
   </article>
 
-<!--End Content-->
 <?	
+	}
 	include "../page/footer.inc.php";
 ?>
