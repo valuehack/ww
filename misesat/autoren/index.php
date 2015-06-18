984717ce5995730eecda774eb22dcf05916b5e3d
bartholos <um@wertewirtschaft.org>
2015-06-18 16:59:16 +0200

    misesat_autor

--- misesat/autoren/index.php
+++ misesat/autoren/index.php
@@ -29,19 +29,12 @@ if(isset($_GET['q']))
       	<section>
       	<h2>Leben</h2>
       
-      	<img src="<?=$img?>" class="denker" alt="Portr&auml;t von <?=$name?>">
-      	
+      <?php
+      	if ($img) { 
+      	echo '<img src="'.$img.'" class="denker" alt="Portr&auml;t von '.$name.'">';
+		}
+      ?>
       	<p><?=$bio?></p>
-      
-      	<div class="booksource">
-      		<p>
-      			<img src="wienerschule_buch.jpg" title="Die Wiener Schule der National&oouml;konomie von Eugen-Maria Schulak und Hebert Unterk&ouml;fler" alt="">
-      		Die hier vorgestellte Biografie enstammt dem Buch <i>&quot;Die Wiener Schule der National&ouml;konomie&quot;</i>, geschrieben von <a href="#">Eugen-Maria Schulak</a> und Hebert Unterk&ouml;fler. Das Werk erschien in der Reihe &quot;Wiener Wissen&quot; und ist die bislang am tiefsten gehende Darstellung der historischen Entwicklung der Wiener Schule der &Ouml;konomie. Umfassende Recherchen bef&ouml;rderten viel Unbekanntes und &Uuml;berraschendes zu Tage. Neben Biografien und der historischen Entwicklung werden auch die Ideen der Wiener Schule leicht verst&auml;ndlich dargestellt.<br><br>
-      		
-      		<b>&quot;Die Wiener Schule der National&ouml;konomie&quot;</b> beim <a href="#">Institut f&uuml;r Wertewirtschaft</a> und bei <a href="#">Amazon.de</a>.
-
-      		</p>
-      	</div>
       	</section>
      	<section class="works">
      	
@@ -73,7 +66,14 @@ if(isset($_GET['q']))
             	</ul>
         	</span>
         <div class="clear"></div>
-       </div>
+              	<div class="booksource">
+      		<p>
+      			<img src="wienerschule_buch.jpg" title="Die Wiener Schule der National&oouml;konomie von Eugen-Maria Schulak und Hebert Unterk&ouml;fler" alt="">
+      		Die hier vorgestellte Biografie enstammt dem Buch <i>&quot;Die Wiener Schule der National&ouml;konomie&quot;</i>, geschrieben von <a href="#">Eugen-Maria Schulak</a> und Hebert Unterk&ouml;fler. Das Werk erschien in der Reihe &quot;Wiener Wissen&quot; und ist die bislang am tiefsten gehende Darstellung der historischen Entwicklung der Wiener Schule der &Ouml;konomie. Umfassende Recherchen bef&ouml;rderten viel Unbekanntes und &Uuml;berraschendes zu Tage. Neben Biografien und der historischen Entwicklung werden auch die Ideen der Wiener Schule leicht verst&auml;ndlich dargestellt.<br><br>     		
+      		<b>&quot;Die Wiener Schule der National&ouml;konomie&quot;</b> beim <a href="#">Institut f&uuml;r Wertewirtschaft</a> und bei <a href="#">Amazon.de</a>.
+      		</p>
+      	</div>
+       </article>
     </div>
 <?php
 }
