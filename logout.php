7193ed7f5fdf5fd43c077a5090b129c8196a387b
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-06-18 16:44:26 +0200

    logout

--- logout.php
+++ logout.php
@@ -6,38 +6,36 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once('../libraries/password_compatibility_library.php');
+    require_once('libraries/password_compatibility_library.php');
 }
 // include the config
-require_once('../config/config.php');
+require_once('config/config.php');
 
 // include the to-be-used language, english by default. feel free to translate your project and include something else
-require_once('../translations/de.php');
+require_once('translations/de.php');
 
 // include the PHPMailer library
-require_once('../libraries/PHPMailer.php');
+require_once('libraries/PHPMailer.php');
 
 // load the login class
-require_once('../classes/Login.php');
-require_once('../classes/Registration.php');
+require_once('classes/Login.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
 $login = new Login();
-$registration = new Registration();
 $title="Logout";
 
 // ... ask if we are logged in here:
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
-    include("../views/index_in.php");
+    include("views/index_in.php");
 
 } 
 
 else {
     // the user is not logged in. you can do whatever you want here.
     
-include("../views/_header_not_in.php");
+include("views/_header_not_in.php");
     
 ?>
 
@@ -46,10 +44,12 @@ include("../views/_header_not_in.php");
         <div class="salon_content">
           
           <h3>Logout erfolgreich</h3>
+
+          <p><a href='index.php'>&raquo; zur Startseite</a></p>
         
         </div>
 </div>
-<? include "../views/_footer.php"; 
+<? include "views/_footer.php"; 
 
 }
 ?>
