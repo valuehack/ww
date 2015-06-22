cae4f322176adcfc7c2ebb07e39b3498d1225db1
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-06-22 11:31:15 +0200

    et

--- views/_header_in.php
+++ views/_header_in.php
@@ -191,7 +191,7 @@ while ($entry = mysql_fetch_array($result))
                 			<li><a href="/abo/profil.php">Profil</a></li>
                 			<li><a href="/abo/upgrade.php">Upgrade</a></li>
                 			<li class="divider"></li>
-                			<li class="dropdown-header dropdown_credits">Guthaben: <?echo $entry[credits_left];?> <span class='et'>&amp;</span></li>
+                			<li class="dropdown-header dropdown_credits">Guthaben: <?echo $entry[credits_left];?> <img class="et_img" src="../style/gfx/scholarium_logo_credits.png"></li>
                 			<li><a href="/abo/korb.php">Warenkorb <span class="badge"><?echo $total_quantity;?></span></a></li> 
                 			<li class="divider"></li>
                 			<li><a href="/logout.php?logout">Abmelden</a></li>        			               		
