66aec2c174b83b075d7a1630dfaaf15bd796f182
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-06-22 15:54:46 +0200

    dropdown coin

--- views/_header_in.php
+++ views/_header_in.php
@@ -191,7 +191,8 @@ while ($entry = mysql_fetch_array($result))
                 			<li><a href="/abo/profil.php">Profil</a></li>
                 			<li><a href="/abo/upgrade.php">Upgrade</a></li>
                 			<li class="divider"></li>
-                			<li class="dropdown-header dropdown_credits">Guthaben: <?echo $entry[credits_left];?> <img style='height:20px' src="../style/gfx/coin.png"></li>
+                			<span class='dropdown_coin'><img src="../style/gfx/coin.png"></span>
+                      <li class="dropdown-header dropdown_credits">Guthaben: <?echo $entry[credits_left];?></li>
                 			<li><a href="/abo/korb.php">Warenkorb <span class="badge"><?echo $total_quantity;?></span></a></li> 
                 			<li class="divider"></li>
                 			<li><a href="/logout.php?logout">Abmelden</a></li>        			               		
