f7d7f6425ef3077a38dc0e937727d773e1f5fa59
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-06-29 11:12:04 +0200

    Projekte coin

--- views/projekte_in.php
+++ views/projekte_in.php
@@ -57,12 +57,12 @@ if ($id = $_GET["q"])
     }
     else {
 		echo "<div class='projekte_invest'>
-		<p>Interessierte Mitglieder haben bereits <span class='projekte_credits_sold'>".$entry[spots_sold]."</span> von <span class='projekte_credits_sold'>".$entry[spots]."</span> n&ouml;tigen Credits investiert.</p>"; ?>
+		<p>Interessierte Mitglieder haben bereits <div class='projekte_price_list'><li><span class='projekte_credits_sold'>".$entry[spots_sold]."</span> von <span class='projekte_credits_sold'>".$entry[spots]."</span> n&ouml;tigen</li> <li class='projekte_coin'><img src='../style/gfx/coin.png'></li></div> investiert.</p>"; ?>
       	<form class="projekte_invest_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         	<input type="hidden" name="add" value="<?php echo $n ?>" />
         	<span class="projekte_invest_span">Ich m&ouml;chte mit </span>
         	<input class="projekte_invest_select" type="number" name="quantity" value="1" min="1" max="<?php echo $avail;?>">
-        	<span class="projekte_invest_span">Credit(s) zu diesem Projekt beitragen.</span><br>
+        	<span class="projekte_invest_span"><span class='projekte_coin'><img src='../style/gfx/coin.png'></span> zu diesem Projekt beitragen.</span><br>
         	<input type="hidden" name="projekt" value="1" />
           <input type="submit" class="medien_inputbutton" value="Jetzt beitragen">
       	</form>
