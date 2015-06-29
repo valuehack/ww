0470ee8050bec6f11276319c4d2a8211f3a82199
bh3005 <Berni@Bernhards-MacBook-Air-2.local>
2015-06-29 11:17:22 +0200

    proejekt

--- views/projekte_in.php
+++ views/projekte_in.php
@@ -57,7 +57,7 @@ if ($id = $_GET["q"])
     }
     else {
 		echo "<div class='projekte_invest'>
-		<p>Interessierte Mitglieder haben bereits <div class='projekte_price_list'><li><span class='projekte_credits_sold'>".$entry[spots_sold]."</span> von <span class='projekte_credits_sold'>".$entry[spots]."</span> n&ouml;tigen</li> <li class='projekte_coin'><img src='../style/gfx/coin.png'></li></div> investiert.</p>"; ?>
+		<p>Interessierte Mitglieder haben bereits <span class='projekte_price_list'><li><span class='projekte_credits_sold'>".$entry[spots_sold]."</span> von <span class='projekte_credits_sold'>".$entry[spots]."</span> n&ouml;tigen</li> <li class='projekte_coin'><img src='../style/gfx/coin.png'></li></span> investiert.</p>"; ?>
       	<form class="projekte_invest_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         	<input type="hidden" name="add" value="<?php echo $n ?>" />
         	<span class="projekte_invest_span">Ich m&ouml;chte mit </span>
