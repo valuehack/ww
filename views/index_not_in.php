492cbc862e86650b6812391d7ad4bf4e813d3fc0
bartholos <um@wertewirtschaft.org>
2015-07-02 16:28:29 +0200

    landing_signup

--- views/index_not_in.php
+++ views/index_not_in.php
@@ -17,7 +17,7 @@
         <!--<link href="../style/modal.css" rel="stylesheet">-->
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="../tools/bootstrap.js"></script>
-
+        
         <!-- this is used for this fancy login form -->
         <script language="javascript" src="/js/jquery.js"></script>
         <script language="javascript" src="/js/script.js"></script>
@@ -77,21 +77,10 @@ if (isset($registration)) {
 
     $ok2 = $_POST['ok2'];
 ?>  
-
-<html>
-    <head>
-        <title>Scholarium</title>
-        <link rel="stylesheet" type="text/css" href="style/landing.css">
-        <link rel="stylesheet" type="text/css" href="style/style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
-        <!-- Bootstrap -->
-        <script src="tools/bootstrap.js"></script>
-        
-    </head>
     <body>
        <header class="landing_header">
                     <div class="anmelden"><button class="landing_login_button" type="button" data-toggle="modal" data-target="#login" value="Anmelden">Anmelden</button></div>
+                    <div class="anmelden"><button class="landing_login_button" type="button" data-toggle="modal" data-target="#signup" value="Anmelden">Eintragen</button></div>
                     
 <!-- Login Modal -->
   <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -109,7 +98,29 @@ if (isset($registration)) {
                 <input class="inputfield_login" id="user_password" type="password" name="user_password" placeholder=" Passwort" required><br>
                 <input class="inputbutton_login" id="inputbutton" name="anmelden_submit" type="submit" value="Anmelden">
             </form>     
-            <p class="password_login"><a href="/password_reset.php">Passwort vergessen?</a></p>     
+            <p class="password_login"><a href="/password_reset.php">Passwort vergessen?</a></p>  
+            <p class="password_login"><a href="#" data-toggle="modal" data-target="#signup">Eintragen</a></p>   
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- Sign Up Modal -->  
+  <div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog-login">
+      <div class="modal-content-login">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <div class="modal-header">
+
+          <h2 class="modal-title" id="myModalLabel">Eintragen</h2>
+        </div>
+        <div class="modal-body">
+          <p>
+            <form method="post" action="index.php" name="registerform">
+                <input class="inputfield_login" id="keyword" type="email" placeholder=" E-Mail Adresse" name="user_email" required><br>
+                <input class="inputfield_login" id="user_password" type="password" name="user_password" placeholder=" Passwort" required><br>
+                <input class="inputbutton_login" id="inputbutton" name="anmelden_submit" type="submit" value="Eintragen">
+            </form>         
           </p>
         </div>
       </div>
