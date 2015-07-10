5bb7a2cbdefd60ae8b00bb879673fd7d4238274e
bartholos <um@wertewirtschaft.org>
2015-07-10 12:31:06 +0200

    footer

--- views/_footer.php
+++ views/_footer.php
@@ -1,7 +1,7 @@
         <footer class="footer">
         	<div class="footer_section">
         		<div class="footer_info">
-					<p>Die Wertewirtschaft ist ein lernendes Unternehmen, in dem Wege werte- und sinnorientierten Unternehmertums praktisch erkundet und theoretisch reflektiert werden. Wir bieten eine Orientierungshilfe f&uuml;r kritische B&uuml;rger und eine Bildungsalternative f&uuml;r junge Menschen, die der heutigen Blasenwirtschaft, aber auch ideologischen Versprechen misstrauen.
+					<p>Das Scholarium ist ein lernendes Unternehmen, in dem Wege werte- und sinnorientierten Unternehmertums praktisch erkundet und theoretisch reflektiert werden. Wir bieten eine Orientierungshilfe f&uuml;r kritische B&uuml;rger und eine Bildungsalternative f&uuml;r junge Menschen, die der heutigen Blasenwirtschaft, aber auch ideologischen Versprechen misstrauen.
         			</p>
         		</div>
         		<div class="footer_contact">
@@ -11,10 +11,9 @@
         				<li>A - 1080 Wien</li>
         				<li>&Ouml;sterreich</li>
         				<li>&nbsp;</li>
-        				<li>Fax:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +43 1 2533033 4733</li>
         				<li>E-Mail: &nbsp;<a href="mailto:&#105;nf&#111;&#064;&#119;&#101;rt&#101;wirtsc&#104;&#097;f&#116;.or&#103;">&#105;nf&#111;&#064;&#119;&#101;rt&#101;wirtsc&#104;&#097;f&#116;.or&#103;</a></li>
 					</ul>
-					<p><a href="../kontakt/">Impressum</a> <a href="../agb/">AGB</a></p> 
+					<p><a href="../kontakt/">Impressum</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="../agb/">AGB</a></p> 
         		</div>
         	</div>
         	<div class="footer_section">
@@ -23,10 +22,10 @@
         		</div>
         		<div class="footer_social">
         			<ul>
-        				<li><a class="footer_social_facebook" href="https://www.facebook.com/wertewirtschaft" target="_blank">Scholarium auf Facebook</a></li>
-        				<li><a class="footer_social_twitter" href="https://www.twitter.com/wertewirtschaft" target="_blank">Follow us on Twitter</a></li>
-        				<li><a class="footer_social_xing" href="https://www.xing.com/companies/institutf%C3%BCrwertewirtschaft" target="_blank">Scholarium bei Xing</a></li>
-        				<li><a class="footer_social_youtube" href="https://www.youtube.com/user/Wertewirtschaft" target="_blank">Scholarium Youtube Channel</a></li>
+        				<li><a class="footer_social_facebook" href="https://www.facebook.com/wertewirtschaft" target="_blank" title="Besuche uns auf Facebook"></a></li>
+        				<li><a class="footer_social_twitter" href="https://www.twitter.com/wertewirtschaft" target="_blank" title="Folge Scholarium auf Twitter"></a></li>
+        				<li><a class="footer_social_xing" href="https://www.xing.com/companies/institutf%C3%BCrwertewirtschaft" target="_blank" title="Scholarium bei Xing"></a></li>
+        				<li><a class="footer_social_youtube" href="https://www.youtube.com/user/Wertewirtschaft" target="_blank" title="Schau unsere Videos auf YouTube"></a></li>
         			</ul>
         		</div>
         	</div>
