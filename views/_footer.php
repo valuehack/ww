6d211310a4941243529fe73e6223975b43f3937e
bartholos <um@wertewirtschaft.org>
2015-07-10 11:36:25 +0200

    footer_and_clean-up

--- views/_footer.php
+++ views/_footer.php
@@ -1,8 +1,34 @@
         <footer class="footer">
-            <p>
-                <a href="http://scholarium.at/">&copy; Scholarium&trade;</a> &nbsp; 
-                <a href="../kontakt/">Impressum</a> &nbsp; 
-                <a href="../agb/">AGB</a> &nbsp; 
+        	<div class="footer_section">
+        		<div class="footer_info">
+					<p>Die Wertewirtschaft ist ein lernendes Unternehmen, in dem Wege werte- und sinnorientierten Unternehmertums praktisch erkundet und theoretisch reflektiert werden. Wir bieten eine Orientierungshilfe f&uuml;r kritische B&uuml;rger und eine Bildungsalternative f&uuml;r junge Menschen, die der heutigen Blasenwirtschaft, aber auch ideologischen Versprechen misstrauen.
+        			</p>
+        		</div>
+        		<div class="footer_contact">
+        			<p><a href="http://scholarium.at/">&copy; Scholarium&trade;</a></p>
+        			<ul>
+        				<li>Schl&ouml;sselgasse 18//</li>
+        				<li>1080 Wien</li>
+        				<li>Fax</li>
+        				<li>E-Mail</li>
+        			</ul>
+        		</div>
+        	</div>
+        	<div class="footer_section">
+        		<div class="footer_logo">
+        			<img src="../style/gfx/footer_logo.png" alt="Scholarium Logo">
+					<a href="../kontakt/">Impressum</a>
+                	<a href="../agb/">AGB</a> 
+        		</div>
+        		<div class="footer_social">
+        			<ul>
+        				<li><a class="footer_social_facebook" href="https://www.facebook.com/wertewirtschaft" target="_blank">Scholarium auf Facebook</a></li>
+        				<li><a class="footer_social_twitter" href="https://www.twitter.com/wertewirtschaft" target="_blank">Follow us on Twitter</a></li>
+        				<li><a class="footer_social_xing" href="https://www.xing.com/companies/institutf%C3%BCrwertewirtschaft" target="_blank">Scholarium bei Xing</a></li>
+        				<li><a class="footer_social_youtube" href="https://www.youtube.com/user/Wertewirtschaft" target="_blank">Scholarium Youtube Channel</a></li>
+        			</ul>
+        		</div>
+        	</div>
         </footer>
     </body>
 </html>
