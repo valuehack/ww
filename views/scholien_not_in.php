042dee95ed1b496dc94a71a246f053814bc5ad1a
bartholos <um@wertewirtschaft.org>
2015-07-13 13:23:47 +0200

    social_links

--- views/scholien_not_in.php
+++ views/scholien_not_in.php
@@ -35,10 +35,11 @@ if(isset($_GET['q']))
 	}
 
 		include('_header_not_in.php');
-?>
+		
+?>		
         <aside class="social">
                    <ul>
-                       <li><a href="https://www.facebook.com/sharer/sharer.php?u=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/facebook.png" alt="Facebook" title="Teile diesen Post auf Facebook!"></a></li>
+                       <li><a href="https://www.facebook.com/sharer/sharer.php?s=100&p[title]=<?=$title?>&p[summary]=<?$decription_fb?>&p[url]=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>&p[images][0]=<?=$img_url?>" target="_blank"><img src="../style/gfx/facebook.png" alt="Facebook" title="Teile diesen Post auf Facebook!"></a></li>
                        <li><a href="http://twitter.com/share?url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>&text=<?php echo $title;?>&via=wertewirtschaft" target="_blank"><img src="../style/gfx/twitter.png" alt="Twitter" title="Tweete diesen Post!"></a></li>
                        <li><a href="https://plus.google.com/share?url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/google.png" alt="Google+" title="Teile diesen Post auf Google+!"></a></li>
                        <li><a href="http://www.linkedin.com/shareArticle?mini=true&url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/linkedin.png" alt="Linkedin" title="Teile diesen Post auf Linkedin!"></a></li>
