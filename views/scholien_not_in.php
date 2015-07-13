f6bcd3e01c1e353d19fb48c89d0fe9aec2b664c3
bartholos <um@wertewirtschaft.org>
2015-07-13 12:12:50 +0200

    social_links

--- views/scholien_not_in.php
+++ views/scholien_not_in.php
@@ -36,7 +36,7 @@ if(isset($_GET['q']))
         <aside class="social">
                    <ul>
                        <li><a href="https://www.facebook.com/sharer/sharer.php?u=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/facebook.png" alt="Facebook" title="Teile diesen Post auf Facebook!"></a></li>
-                       <li><a href="http://twitter.com/share?url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>&text=<?php echo $id;?>&hashtags=<?php echo $id;?>" target="_blank"><img src="../style/gfx/twitter.png" alt="Twitter" title="Tweete diesen Post!"></a></li>
+                       <li><a href="http://twitter.com/share?url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>&text=<?php echo $id;?>&via=wertewirtschaft" target="_blank"><img src="../style/gfx/twitter.png" alt="Twitter" title="Tweete diesen Post!"></a></li>
                        <li><a href="https://plus.google.com/share?url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/google.png" alt="Google+" title="Teile diesen Post auf Google+!"></a></li>
                        <li><a href="http://www.linkedin.com/shareArticle?mini=true&url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/linkedin.png" alt="Linkedin" title="Teile diesen Post auf Linkedin!"></a></li>
                        <li><a href="https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url=http://scholarium.at/scholien/index.php?id=<?php echo $id;?>" target="_blank"><img src="../style/gfx/xing.png" alt="Facebook" title="Teile diesen Post auf Xing!"></a></li>
@@ -50,7 +50,7 @@ if(isset($_GET['q']))
 	echo "</header>";
 	echo "<p class='linie'><img src='../style/gfx/linie.png' alt=''></p>";
 	echo "<p class='blogdate'><!--Keyword: ".$id."&nbsp &nbsp &nbsp-->".date('d.m.Y', strtotime($publ_date))."</p>";
-	echo "<img class='blog_img' src='$img_url' alt='$id''>";
+	echo "<img class='blog_img' rel='image_src' src='$img_url' alt='$id''>";
 	echo "<div class='blog_text'>";
 	echo $public;
 	echo "</div>";
