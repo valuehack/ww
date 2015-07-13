73b81d83586c46758d62bb64dc6bc893223b3c47
bartholos <um@wertewirtschaft.org>
2015-07-13 14:38:09 +0200

    scholien

--- views/scholien_not_in.php
+++ views/scholien_not_in.php
@@ -52,9 +52,9 @@ if(isset($_GET['q']))
 	echo "<header>";
 	echo "<h1>$title</h1>";
 	echo "</header>";
-	echo "<p class='linie'><img src='../style/gfx/linie.png' alt=''></p>";
 	echo "<p class='blogdate'><!--Keyword: ".$id."&nbsp &nbsp &nbsp-->".date('d.m.Y', strtotime($publ_date))."</p>";
 	echo "<img class='blog_img' src='$img_url' alt='$id'>";
+	echo "<p class='linie'><img src='../style/gfx/linie.png' alt=''></p>";
 	echo "<div class='blog_text'>";
 	echo $public;
 	echo "</div>";
