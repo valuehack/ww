71176f442bf60fac7f9a8d0374cbe5a75a17ae8f
bartholos <um@wertewirtschaft.org>
2015-07-16 13:55:54 +0200

    ticket

--- htmltopdf.php
+++ htmltopdf.php
@@ -1,7 +1,9 @@
 <?php
 require_once("dompdf/dompdf_config.inc.php");
 
-$html = file_get_contents("ticket.html");
+$name = $_GET[q];
+
+$html = file_get_contents($name.".html");
 
 $dompdf = new DOMPDF();
 $dompdf->load_html($html);
