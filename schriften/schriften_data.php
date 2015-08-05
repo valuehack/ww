4fe8224974d03a8be85263b0361b963a4e2b2dc1
bartholos <um@wertewirtschaft.org>
2015-08-05 18:28:49 +0200

    schriften_org

--- schriften/schriften_data.php
+++ schriften/schriften_data.php
@@ -14,7 +14,7 @@
   $total_pages = $total_pages[num];
   
   /* Setup vars for query. */
-  $targetpage = "index.php?type=$type3&";  //your file name  (the name of this file)
+  $targetpage = "index.php?type=$type3";  //your file name  (the name of this file)
   $limit = 10;                //how many items to show per page
   $page = $_GET['page'];
   if($page) 
@@ -45,7 +45,7 @@
     $pagination .= "<div class=\"pagination\">";
     //previous button
     if ($page > 1) 
-      $pagination.= "<a href=\"$targetpagepage=$prev\">&laquo; zur&uuml;ck</a>";
+      $pagination.= "<a href=\"$targetpage&page=$prev\">&laquo; zur&uuml;ck</a>";
     else
       $pagination.= "<span class=\"disabled\">&laquo; zur&uuml;ck</span>";  
     
@@ -57,7 +57,7 @@
         if ($counter == $page)
           $pagination.= "<span class=\"current\">$counter</span>";
         else
-          $pagination.= "<a href=\"$targetpagepage=$counter\">$counter</a>";         
+          $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
       }
     }
     elseif($lastpage > 5 + ($adjacents * 2))  //enough pages to hide some
@@ -70,48 +70,48 @@
           if ($counter == $page)
             $pagination.= "<span class=\"current\">$counter</span>";
           else
-            $pagination.= "<a href=\"$targetpagepage=$counter\">$counter</a>";         
+            $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
         }
         $pagination.= "...";
-        $pagination.= "<a href=\"$targetpagepage=$lpm1\">$lpm1</a>";
-        $pagination.= "<a href=\"$targetpagepage=$lastpage\">$lastpage</a>";   
+        $pagination.= "<a href=\"$targetpage&page=$lpm1\">$lpm1</a>";
+        $pagination.= "<a href=\"$targetpage&page=$lastpage\">$lastpage</a>";   
       }
       //in middle; hide some front and some back
       elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
       {
-        $pagination.= "<a href=\"$targetpagepage=1\">1</a>";
-        $pagination.= "<a href=\"$targetpagepage=2\">2</a>";
+        $pagination.= "<a href=\"$targetpage&page=1\">1</a>";
+        $pagination.= "<a href=\"$targetpage&page=2\">2</a>";
         $pagination.= "...";
         for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
         {
           if ($counter == $page)
             $pagination.= "<span class=\"current\">$counter</span>";
           else
-            $pagination.= "<a href=\"$targetpagepage=$counter\">$counter</a>";         
+            $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
         }
         $pagination.= "...";
-        $pagination.= "<a href=\"$targetpagepage=$lpm1\">$lpm1</a>";
-        $pagination.= "<a href=\"$targetpagepage=$lastpage\">$lastpage</a>";   
+        $pagination.= "<a href=\"$targetpage&page=$lpm1\">$lpm1</a>";
+        $pagination.= "<a href=\"$targetpage&page=$lastpage\">$lastpage</a>";   
       }
       //close to end; only hide early pages
       else
       {
-        $pagination.= "<a href=\"$targetpagepage=1\">1</a>";
-        $pagination.= "<a href=\"$targetpagepage=2\">2</a>";
+        $pagination.= "<a href=\"$targetpage&page=1\">1</a>";
+        $pagination.= "<a href=\"$targetpage&page=2\">2</a>";
         $pagination.= "...";
         for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
         {
           if ($counter == $page)
             $pagination.= "<span class=\"current\">$counter</span>";
           else
-            $pagination.= "<a href=\"$targetpagepage=$counter\">$counter</a>";         
+            $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
         }
       }
     }
     
     //next button
     if ($page < $counter - 1) 
-      $pagination.= "<a href=\"$targetpagepage=$next\">vor &raquo;</a>";
+      $pagination.= "<a href=\"$targetpage&page=$next\">vor &raquo;</a>";
     else
       $pagination.= "<span class=\"disabled\">vor &raquo;</span>";
     $pagination.= "</div>\n";   
