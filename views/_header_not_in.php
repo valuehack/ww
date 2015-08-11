63fcb063ebb699bb6397820f4a914edb8888e72c
bartholos <um@wertewirtschaft.org>
2015-08-11 14:21:36 +0200

    autocomplete

--- views/_header_not_in.php
+++ views/_header_not_in.php
@@ -123,7 +123,7 @@ mysql_query("SET time_zone = 'Europe/Vienna'");
         <div class="modal-body">
           <p>
           	<form method="post" action="index.php" name="registerform" autocomplete="on">
-          		<input class="inputfield_login" id="keyword" type="email" placeholder=" E-Mail Adresse" name="user_email" required><br>
+          		<input class="inputfield_login" id="keyword" type="email" placeholder=" E-Mail Adresse" name="user_email" autocomplete="on" required><br>
           		<input class="inputfield_login" id="user_password" type="password" name="user_password" placeholder=" Passwort" required><br>
           		<input class="inputbutton_login" id="inputbutton" name="anmelden_submit" type="submit" value="Anmelden">
           	</form>     
