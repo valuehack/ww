2a528f045c682978cde4d21ac4782b69fa960877
bartholos <um@wertewirtschaft.org>
2015-08-21 19:16:42 +0200

    medien_layout

---
+++ medien/medien_data.php
@@ -0,0 +1,180 @@
+
+    <div class="salon_types">
+    	<span><a href="index.php">Alle</a></span>
+    	<span><a <?if ($type2 == 'oekonomik'){echo 'class="salon_types_active"';}?> href="?type=oekonomik">&Oumlt;konomik</a></span>
+    	<span><a <?if ($type2 == 'ethik'){echo 'class="salon_types_active"';}?> href="?type=ethik">Ethik</a></span>
+    	<span><a <?if ($type2 == 'politik'){echo 'class="salon_types_active"';}?> href="?type=politik">Politik</a></span>
+    	<span><a <?if ($type2 == 'audio'){echo 'class="salon_types_active"';}?> href="?type=audio">Tonaufnahmen</a></span>
+    </div> 
+    <div class="medien_content">
+<?php
+		$type3 = 'Wiener Schule der ';
+
+
+//Pagination Script found at http://www.phpeasystep.com/phptu/29.html
+  $tbl_name="produkte";   //your table name
+  // How many adjacent pages should be shown on each side?
+  $adjacents = 2;
+  
+  /* 
+     First get total number of rows in data table. 
+     If you have a WHERE clause in your query, make sure you mirror it here.
+  */
+  $query = "SELECT COUNT(*) as num FROM $tbl_name WHERE (SUBSTRING(type,0,18) LIKE '$type3') AND status > 0";
+  $total_pages = mysql_fetch_array(mysql_query($query));
+  $total_pages = $total_pages[num];
+  
+  /* Setup vars for query. */
+  $targetpage = "index.php?type=$type2";  //your file name  (the name of this file)
+  $limit = 6;                //how many items to show per page
+  $page = $_GET['page'];
+  if($page) 
+    $start = ($page - 1) * $limit;      //first item to display on this page
+  else
+    $start = 0;               //if no page var is given, set start to 0
+  
+  /* Get data. */
+  $sql = "SELECT * from produkte WHERE (type LIKE '$type3') AND status > 0 order by n desc LIMIT $start, $limit";
+  
+  $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+  
+
+    /* Setup page vars for display. */
+  if ($page == 0) $page = 1;          //if no page var is given, default to 1.
+  $prev = $page - 1;              //previous page is page - 1
+  $next = $page + 1;              //next page is page + 1
+  $lastpage = ceil($total_pages/$limit);    //lastpage is = total pages / items per page, rounded up.
+  $lpm1 = $lastpage - 1;            //last page minus 1
+  
+  /* 
+    Now we apply our rules and draw the pagination object. 
+    We're actually saving the code to a variable in case we want to draw it more than once.
+  */
+  $pagination = "";
+  if($lastpage > 1)
+  { 
+    $pagination .= "<div class=\"pagination\">";
+    //previous button
+    if ($page > 1) 
+      $pagination.= "<a href=\"$targetpage&page=$prev\">&laquo; zur&uuml;ck</a>";
+    else
+      $pagination.= "<span class=\"disabled\">&laquo; zur&uuml;ck</span>";  
+    
+    //pages 
+    if ($lastpage < 7 + ($adjacents * 2)) //not enough pages to bother breaking it up
+    { 
+      for ($counter = 1; $counter <= $lastpage; $counter++)
+      {
+        if ($counter == $page)
+          $pagination.= "<span class=\"current\">$counter</span>";
+        else
+          $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
+      }
+    }
+    elseif($lastpage > 5 + ($adjacents * 2))  //enough pages to hide some
+    {
+      //close to beginning; only hide later pages
+      if($page < 1 + ($adjacents * 2))    
+      {
+        for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+        {
+          if ($counter == $page)
+            $pagination.= "<span class=\"current\">$counter</span>";
+          else
+            $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
+        }
+        $pagination.= "...";
+        $pagination.= "<a href=\"$targetpage&page=$lpm1\">$lpm1</a>";
+        $pagination.= "<a href=\"$targetpage&page=$lastpage\">$lastpage</a>";   
+      }
+      //in middle; hide some front and some back
+      elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+      {
+        $pagination.= "<a href=\"$targetpage&page=1\">1</a>";
+        $pagination.= "<a href=\"$targetpage&page=2\">2</a>";
+        $pagination.= "...";
+        for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+        {
+          if ($counter == $page)
+            $pagination.= "<span class=\"current\">$counter</span>";
+          else
+            $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
+        }
+        $pagination.= "...";
+        $pagination.= "<a href=\"$targetpage&page=$lpm1\">$lpm1</a>";
+        $pagination.= "<a href=\"$targetpage&page=$lastpage\">$lastpage</a>";   
+      }
+      //close to end; only hide early pages
+      else
+      {
+        $pagination.= "<a href=\"$targetpage&page=1\">1</a>";
+        $pagination.= "<a href=\"$targetpage&page=2\">2</a>";
+        $pagination.= "...";
+        for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+        {
+          if ($counter == $page)
+            $pagination.= "<span class=\"current\">$counter</span>";
+          else
+            $pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";         
+        }
+      }
+    }
+    
+    //next button
+    if ($page < $counter - 1) 
+      $pagination.= "<a href=\"$targetpage&page=$next\">vor &raquo;</a>";
+    else
+      $pagination.= "<span class=\"disabled\">vor &raquo;</span>";
+    $pagination.= "</div>\n";   
+  }
+?>
+
+<table class='schriften_table'>
+<?php
+		//$sql = "SELECT * from produkte WHERE (type LIKE '$type3') AND status > 0 order by n desc";
+		//$result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
+			
+while($entry = mysql_fetch_array($result))
+{
+	$id = $entry[id];
+	
+		      	//check, if there is a image in the salon folder
+	$img = 'http://test.wertewirtschaft.net/schriften/'.$id.'.jpg';
+
+	if (@getimagesize($img)) {
+	    $img_url = $img;
+	} else {
+	    $img_url = "http://test.wertewirtschaft.net/schriften/default.jpg";
+	}
+	
+?>
+		<tr>
+			<td class="schriften_table_a">
+				<a href="<? echo "?q=$id";?>"><img src="<?echo $img_url;?>" alt="Cover <?echo $id;?>"></a>
+			</td>			
+			<td class="schriften_table_b">
+				<span><? echo ucfirst($entry[type]);?></span><br>
+      			<? echo "<a href='?q=$id'>".$entry[title]." </a>"; ?>
+      			<p>
+      				<? if (strlen($entry[text]) > 300) {
+							echo substr ($entry[text], 0, 300);
+              echo '...';
+						}
+						else {
+							echo $entry[text];
+						}
+					?>
+				</p>
+			</td>
+			<!--<td class="schriften_table_c">	
+				<input type="button" class="inputbutton" value="Bestellen / Herunterladen" data-toggle="modal" data-target="#myModal">
+			</td>-->
+		</tr>
+
+    <tr><td colspan="3"> <div class="centered"><p class='linie'><img style='height: 35px' src='../style/gfx/linie.png' alt=''></p></div></td></tr>
+
+
+<?php
+	}
+	echo "</table>";
+	echo $pagination;
