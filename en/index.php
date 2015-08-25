30a70e08b2db39269b043a96fc09f024b79f437f
bartholos <um@wertewirtschaft.org>
2015-08-25 18:11:14 +0200

    craftprobe_update

--- en/index.php
+++ en/index.php
@@ -17,20 +17,20 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once('libraries/password_compatibility_library.php');
+    require_once('../libraries/password_compatibility_library.php');
 }
 // include the config
-require_once('config/config.php');
+require_once('../config/config.php');
 
 // include the to-be-used language, english by default. feel free to translate your project and include something else
-require_once('translations/de.php');
+require_once('../translations/de.php');
 
 // include the PHPMailer library
-require_once('libraries/PHPMailer.php');
+require_once('../libraries/PHPMailer.php');
 
 // load the login class
-require_once('classes/Login.php');
-require_once('classes/Registration.php');
+require_once('../classes/Login.php');
+require_once('../classes/Registration.php');
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
@@ -52,10 +52,10 @@ if ($login->isUserLoggedIn() == true)
 	$test = $entry[test];
 	
 	if ($test == ''){
-		include("views/upgrade_in_temp.php");
+		include("../views/upgrade_in_temp.php");
 	}
 	elseif ($test == 1){
-    	include("views/index_in.php");
+    	include("../views/index_in.php");
 	}
 } 
 else 
