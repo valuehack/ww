e18186c103665580771ecad46e124f65bf69276e
bartholos <um@wertewirtschaft.org>
2015-08-25 18:27:51 +0200

    new_en_setup

--- en/index.php
+++ en/index.php
@@ -69,12 +69,12 @@ else
 		<title>Welcome | Scholarium</title>
     
     	<link rel="shortcut icon" href="/favicon.ico">
-    	<link rel="stylesheet" type="text/css" href="../../style/style.css">
+    	<link rel="stylesheet" type="text/css" href="www.scholarium.at/style/style.css">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="../tools/bootstrap.js"></script>
+		<script src="www.scholarium.at/tools/bootstrap.js"></script>
 
     	<!-- this is used for this fancy login form -->
     	<script language="javascript" src="../js/jquery.js"></script>
