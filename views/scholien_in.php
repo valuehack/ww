9215d297e2bec27f5104ab696212d586e7410b77
bartholos <um@wertewirtschaft.org>
2015-08-25 14:39:16 +0200

    access_changes

--- views/scholien_in.php
+++ views/scholien_in.php
@@ -68,7 +68,7 @@ if(isset($_GET['q']))
 		echo "</header>";
 		}	
 		
-	if ($_SESSION['Mitgliedschaft'] == 1) {
+	if ($_SESSION['Mitgliedschaft'] >= 1) {
 		"<p class='blogdate'>".date('d.m.Y', strtotime($publ_date))."</p>";
 		?>
 		<!--<img class="blog_img" src="<?echo $img_url;?>" rel="image_src" alt="<?echo $id;?>">-->
