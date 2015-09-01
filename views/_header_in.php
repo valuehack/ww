bcb6e14e8afe99f797be515a978a8251066024d8
bartholos <um@wertewirtschaft.org>
2015-09-01 18:37:00 +0200

    nav

--- views/_header_in.php
+++ views/_header_in.php
@@ -237,7 +237,7 @@ while ($entry = mysql_fetch_array($result))
                 <ul id="nav">
                     <li><a href="/scholien/">Scholien</a></li>
                     <li><a id="drop1" data-toggle="dropdown" href="#" data-target="#" role="button" aria-haspopup="true" aria-expanded="false">Veranstaltungen</a>
-                    	<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="drop1">
+                    	<ul class="dropdown-menu" aria-labelledby="drop1">
                     		<li><a href="/veranstaltungen/">Alle</a></li>
                     		<li><a href="/salon/">Salons</a></li>
                     		<li><a href="/seminare/">Seminare</a></li>
