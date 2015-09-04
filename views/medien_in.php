3984e449ab9245ff4282d0feb19c6009a50f3019
Rahim <rt@wertewirtschaft.org>
2015-09-04 20:42:05 +0200

    Modal text Medien

--- views/medien_in.php
+++ views/medien_in.php
@@ -349,9 +349,9 @@ while($entry = mysql_fetch_array($result))
         <h2 class="modal-title" id="myModalLabel">Herunterladen</h2>
       </div>
       <div class="modal-body">
-      	<?php
-      		echo $entry4[modal];
-		?>
+      	
+      		<p>Wir freuen uns, dass Sie eine unserer Medien bestellen m&ouml;chten. Allerdings sind einige Aufnahmen nicht f&uuml;r die &Ouml;ffentlichkeit bestimmt. Unser Webshop, &uuml;ber den alle Medien heruntergeladen werden k&ouml;nnen, steht nur unseren G&auml;sten zur Verf&uuml;gung, die einen kleinen Kostenbeitrag (6,25&euro; im Monat) f&uuml;r das Bestehen des <i>scholarium</i> leisten (und daf&uuml;r die meisten Medien kostenlos beziehen k&ouml;nnen). K&ouml;nnen Sie sich das leisten? Dann folgen Sie diesem Link und in K&uuml;rze erhalten Sie Zugriff auf unsere Schriften:&nbsp;</p>
+		
       </div>
       <div class="modal-footer">
          <a href="../abo/upgrade.php"><button type="button" class="inputbutton">Besuchen Sie uns als Gast</button></a>
