910ab20abd11d2b6dcf58400dcda25ba095fbb63
daneas <dzainius@gmail.com>
2015-09-18 00:31:56 +0200

    reg issue

--- abo/index.php
+++ abo/index.php
@@ -19,10 +19,11 @@ require_once('../libraries/PHPMailer.php');
 
 // load the login class
 require_once('../classes/Login.php');
-
+require_once('classes/Registration.php');
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process.
 $login = new Login();
+$registration = new Registration();
 
 // ... ask if we are logged in here:
 if ($login->isUserLoggedIn() == true) {
