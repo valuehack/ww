e6bdc0c4bcd83799c4f25ce5522ca8e63e1f08ac
bartholos <um@wertewirtschaft.org>
2015-11-11 11:46:31 +0100

    misesat_temp_landing

--- misesat/page/index.php
+++ misesat/page/index.php
@@ -1,15 +1,54 @@
-<? 
-	include "config/header1.inc.php";
+<!DOCTYPE html >
+<html lang="de">	
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    	<meta name="description" content="Verlag und Kompendium zur &Ouml;stereichischen Schule der &Ouml;konomik">
+    	<link rel="shortcut icon" href="../favicon.ico">
+    	<!--[if IE]>
+      		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+    	<![endif]-->
+    	<link rel="stylesheet" href="../style/style.css">
+    	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300|EB+Garamond' rel='stylesheet' type='text/css'>
+    	<title>Willkommen | Mises Austria</title>
+    	<style>
+    		body {
+    			background: rgb(255,255,255);
+    		}
+    		.wrapper {
+    			background: rgb(46, 74, 98);
+    			border-bottom: 0.5em solid rgb(28,109,186);
+    			padding: 40% 0;
+    			text-align: center;
+    		}
+    		@media screen and (min-width: 410px) {
+    			.wrapper {
+    				padding: 20% 0;
+    			}
+    		}
+    		@media screen and (min-width: 50em) {
+    			.wrapper {
+    				padding: 15% 0;
+    			}
+    		}
+    		img {
+    			width: 90%;
+    		}
+    		@media screen and (min-width: 410px) {
+    			img {
+    				width: 70%;
+    			}
+    		}
+    		@media screen and (min-width: 50em) {
+    			img {
+    				width: 40%;
+    			}
+    		}
+    	</style>
+	</head>
+	<body>
+		<div class="wrapper">
+			<img src="../style/ma_logo.svg"">
+		</div> 	
+    </body>
+</html>   
 
-	$title="Willkommen!";
-
-	include "header2.inc.php";
-?>
-
-    <div id="content">
-		<h1>Willkommen bei Mises Austria</h1>
-    </div>    
-
-<?	
-	include "footer.inc.php";
-?>
---
+++ misesat/page/index_org.php
@@ -0,0 +1,15 @@
+<? 
+	include "config/header1.inc.php";
+
+	$title="Willkommen!";
+
+	include "header2.inc.php";
+?>
+
+    <div id="content">
+		<h1>Willkommen bei Mises Austria</h1>
+    </div>    
+
+<?	
+	include "footer.inc.php";
+?>
