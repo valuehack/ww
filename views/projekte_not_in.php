93e824fd9bd0a19c6021d705d11cccf34eff2b15
WerteGK1 <gk@wertewirtschaft.org>
2015-11-23 17:26:50 +0100

    wording temporary workaround for projekte

--- views/projekte_not_in.php
+++ views/projekte_not_in.php
@@ -365,7 +365,9 @@ else {
     		<input type="radio" class="profil_radio" name="projekte_profile[zahlung]" value="kredit">Paypal<br>
     		<input type="radio" class="profil_radio" name="projekte_profile[zahlung]" value="bar">Bar<br>
 
-			<p>Mit dem Klick auf <i>Investieren</i> best&auml;tigen Sie, dass Sie unsere AGB gelesen haben und anerkennen. <a href="../agb/agb.html" onclick="openpopup(this.href); return false">Unsere AGB finden Sie hier.</a></p>
+			<p>Mit dem Klick auf <i>Investieren</i> best&auml;tigen Sie, dass Sie unsere AGB gelesen haben und anerkennen. <a href="../agb/agb.html" onclick="openpopup(this.href); return false">Unsere AGB finden Sie hier.</a><br>
+				Nachdem Sie Ihre Anmeldung &uuml;ber den Link in der Ihnen zugesandten E-Mail abgeschlossen haben, k&ouml;nnen Sie unter Projekte das von Ihnen pr&auml;ferierte Projekt unterst&uuml;tzen. Vielen Dank!
+			</p>
 			
 			<input type=hidden name="projekte_profile[first_reg]" value="projekte">
 			<input type="submit" class="profil_inputbutton" name="register_projekte_from_outside_submit" value="Investieren">
