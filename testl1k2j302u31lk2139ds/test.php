a6d167787c14aebf0523a7a489c83f30afb192fe
daneas <dzainius@gmail.com>
2016-01-21 11:46:58 +0100

    test

--- testl1k2j302u31lk2139ds/test.php
+++ testl1k2j302u31lk2139ds/test.php
@@ -1,13 +1,12 @@
 <?php 
+session_start();
 
 echo "Here is some data for from Paypal:<br>";
 
-session_start();
+// $_SESSION[ipn_post_data] = "test";
 
-$_SESSION[ipn_post_data] = "test";
+$_SESSION[ipn_post_data] = $_POST;
 
-// $_SESSION[ipn_post_data] = $_POST;
-
-print_r($_SESSION);
+print_r($_SESSION[ipn_post_data]);
 
 ?>
