6c9a887d143150544374648202adcdff607388cd
daneas <dzainius@gmail.com>
2016-01-21 11:44:29 +0100

    test

--- testl1k2j302u31lk2139ds/test.php
+++ testl1k2j302u31lk2139ds/test.php
@@ -1,6 +1,13 @@
-sveikas
-
 <?php 
 
+echo "Here is some data for from Paypal:<br>";
+
+session_start();
+
+$_SESSION[ipn_post_data] = "test";
+
+// $_SESSION[ipn_post_data] = $_POST;
+
+print_r($_SESSION);
 
 ?>
