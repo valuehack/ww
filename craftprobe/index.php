f2bfd471b34c7dfdcc8dbb2836cce7be3614ee85
Rahim <rt@wertewirtschaft.org>
2016-02-16 10:36:31 +0100

    Elterninfo cp

--- craftprobe/index.php
+++ craftprobe/index.php
@@ -69,7 +69,7 @@ mail ("info@scholarium.at","craftprobe Anmeldung","$firstname, $name, $email hat
 			
     </head>
     <body>
-		<span id="top"></span>
+		<span id="top" style="font-size:10px;text-align:right"><a href="http://scholarium.at/eltern.php">(&rarr;deutschsprachige Information f&uuml;r Eltern)</a></span>
         <header class="header">
             <p>craft<span>probe</span></p>
             <ul class="nav">
