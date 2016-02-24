aa2965a483282d1644427c73ad33f90e1c2c62ff
bartholos <um@wertewirtschaft.org>
2016-02-24 22:06:41 +0100

    cpFormChange

--- craftprobe/header.php
+++ craftprobe/header.php
@@ -10,6 +10,9 @@ mysql_select_db(DB_WERTE_NAME);
 $pdocon->db_connection = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME . ';charset=latin1', DB_USER, DB_PASS);
 return true;
 
+$wwalt->db_connection = new PDO('mysql:host='. DB_WERTE_HOST .';dbname='. DB_WERTE_NAME . ';charset=utf8', DB_WERTE_USER, DB_WERTE_PASS);
+return true;
+
 header('Content-Type: text/html; charset=ISO-8859-1');
 ?>
 
