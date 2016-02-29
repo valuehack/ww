0d6f7d82722aa0e882e3f0025eaeed7151d94a1d
WerteGK1 <gk@wertewirtschaft.org>
2016-02-29 15:44:59 +0100

    mises.at temporäre startseite überarbeitet

--- misesat/page/index.php
+++ misesat/page/index.php
@@ -11,6 +11,73 @@
     	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300|EB+Garamond' rel='stylesheet' type='text/css'>
     	<title>Willkommen | mises.at</title>
     	<style>
+    	
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_regular.otf') format('opentype');
+}
+
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_italic.otf') format('opentype');
+	font-style: italic;
+}
+
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_bold.otf') format('opentype');
+	font-weight: bold;
+}
+
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_semibold.otf') format('opentype');
+	font-weight: 500;
+}
+
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_italic.otf') format('opentype');
+	font-style: italic;
+}
+
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_bold-italic.otf') format('opentype');
+	font-weight: bold;
+	font-style: italic;
+}
+
+@font-face {
+	font-family: 'Garamond';
+	src: url('http://www.scholarium.at/style/font/garamond_semibold-italic.otf') format('opentype');
+	font-weight: 500;
+	font-style: italic;
+}
+
+@font-face {
+	font-family: 'Futura Book';
+	src: url('http://www.scholarium.at/style/font/futura_regular.ttf') format('truetype');
+}
+
+@font-face {
+	font-family: 'Futura Book';
+	src: url('http://www.scholarium.at/style/font/futura_italic.ttf') format('truetype');
+	font-style: italic;
+}
+
+@font-face {
+	font-family: 'Futura Book';
+	src: url('http://www.scholarium.at/style/font/futura_bold.ttf') format('truetype');
+	font-weight: bold;
+}
+
+@font-face {
+	font-family: 'Futura Book';
+	src: url('http://www.scholarium.at/style/font/futura_bold-italic.ttf') format('truetype');
+	font-style: italic;
+	font-weight: bold;
+}
     		body {
     			background: rgb(255,255,255);
     		}
@@ -43,6 +110,26 @@
     				width: 114px;
     			}
     		}
+    		
+    		b {
+    			font-family: "Futura Book", Helvetica, Arial, serif;
+    			text-align: justify;
+    		}
+    		
+    		tr {
+    			font-family: "Garamond", Georgia, Times, sans-serif;
+    			text-align: justify;
+    		}
+    		
+    		td {
+    			vertical-align: top;
+    			padding: 30px 10px;
+    		}
+    		
+    		td a {
+    			text-decoration: none;
+    			color: inherit;
+    		}
 
     	</style>
 	</head>
@@ -50,16 +137,32 @@
 		<div class="wrapper">
 			<img src="../style/ma_logo.png">
 		</div> 	
-		<h1>Verlagsprogramm</h1>
+		<h1 font-family: "Futura Book", Helvetica, Arial, serif !important>Verlagsprogramm</h1>
 		<div align="center"><table style="width:80%;"><tr><td valign="top"><a rel="nofollow" href="http://www.amazon.de/gp/product/3902639296/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639296&linkCode=as2&tag=scholarium-21"><img border="0" src="http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=3902639296&Format=_SL250_&ID=AsinImage&MarketPlace=DE&ServiceVersion=20070822&WS=1&tag=scholarium-21" ></a><img src="http://ir-de.amazon-adsystem.com/e/ir?t=scholarium-21&l=as2&o=3&a=3902639296" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
 </td>
-<td valign="top"><b>Hans Hermann Hoppe: &Ouml;konomik als Wissenschaft und die Methode der &Ouml;sterreichischen Schule</b><br>Erstmals in deutscher &Uuml;bersetzung liegt hiermit die fr&uuml;he Schrift &quot;Economic Science and the Austrian Method&quot; des &Ouml;konomen und Philosophen Hans Hermann Hoppe vor. Aufbauend auf den Werken von Ludwig von Mises entwickelt Hoppe ein Verst&auml;ndnis der &Ouml;konomik als einer apriorischen Wissenschaft vom menschlichen Handeln. Ein grundlegender Beitrag zur Methodik der &Ouml;sterreichischen Schule und ein Klassiker der Wissenschaftstheorie und Epistemologie.</td></tr>
+<td valign="top"><a href="http://www.amazon.de/gp/product/3902639296/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639296&linkCode=as2&tag=scholarium-21"><b>Hans Hermann Hoppe: &Ouml;konomik als Wissenschaft und die Methode der &Ouml;sterreichischen Schule</b></a><br><br>Erstmals in deutscher &Uuml;bersetzung liegt hiermit die fr&uuml;he Schrift &quot;Economic Science and the Austrian Method&quot; des &Ouml;konomen und Philosophen Hans Hermann Hoppe vor. Aufbauend auf den Werken von Ludwig von Mises entwickelt Hoppe ein Verst&auml;ndnis der &Ouml;konomik als einer apriorischen Wissenschaft vom menschlichen Handeln. Ein grundlegender Beitrag zur Methodik der &Ouml;sterreichischen Schule und ein Klassiker der Wissenschaftstheorie und Epistemologie.</td></tr>
 <tr><td valign="top"><a rel="nofollow" href="http://www.amazon.de/gp/product/3902639334/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639334&linkCode=as2&tag=scholarium-21"><img border="0" src="http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=3902639334&Format=_SL250_&ID=AsinImage&MarketPlace=DE&ServiceVersion=20070822&WS=1&tag=scholarium-21" ></a><img src="http://ir-de.amazon-adsystem.com/e/ir?t=scholarium-21&l=as2&o=3&a=3902639334" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
 </td>
-<td valign="top"><b>Taghizadegan/Stoeferle/Valek/Blasnik: Austrian School for Investors: Austrian Investing between Inflation and Deflation</b><br>Insanity seems to rule the financial markets and many investors are driven by delusion and anxiety. May everything you thought you knew about investing be wrong? 
+<td valign="top"><a href="http://www.amazon.de/gp/product/3902639334/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639334&linkCode=as2&tag=scholarium-21"><b>Taghizadegan/Stoeferle/Valek/Blasnik: Austrian School for Investors: Austrian Investing between Inflation and Deflation</b></a><br><br>Insanity seems to rule the financial markets and many investors are driven by delusion and anxiety. May everything you thought you knew about investing be wrong? 
 The Austrian School's approach provides the needed respite for investors caught in inflationary treadmills.
 Conventional investment experts often overlook economic developments which may become a hazard for mainstream investors. Instead, the Austrian School of Economics has proved itself as an independent approach beyond the interests of politicians and bankers. 
-The financial system is shaking. This book presents new paths through the shaky grounds between the tectonic plates of inflation and deflation to both private and professional investors.</td></tr></table>
-    </div></body>
+The financial system is shaking. This book presents new paths through the shaky grounds between the tectonic plates of inflation and deflation to both private and professional investors.</td></tr>
+	<tr><td valign="top"><a rel="nofollow" href="http://www.amazon.de/gp/product/3902639369/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639369&linkCode=as2&tag=scholarium-21"><img border="0" src="http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=3902639369&Format=_SL250_&ID=AsinImage&MarketPlace=DE&ServiceVersion=20070822&WS=1&tag=scholarium-21" ></a><img src="http://ir-de.amazon-adsystem.com/e/ir?t=scholarium-21&l=as2&o=3&a=3902639369" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+</td>
+<td valign="top"><a href="http://www.amazon.de/gp/product/3902639369/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639369&linkCode=as2&tag=scholarium-21"><b>Ayn Rand: F&uuml;r den neuen Intellektuellen: Eine Streitschrift gegen die pseudointellektuellen Verf&uuml;hrer in den Medien und Universit&auml;ten</b></a><br><br>Wer werden die Neuen Intellektuellen sein?<br><br>
+&bdquo;Jeder Mann und jede Frau, der oder die willens ist, zu denken. All jene, die wissen, dass das Leben des Menschen von der Vernunft geleitet werden muss; jene, die ihr eigenes Leben wertsch&auml;tzen und nicht willens sind, es dem Kult der Verzweiflung im heutigen Dschungel zynischer Ohnmacht auszuliefern, so wie sie nicht willens sind, die Welt den Dunklen Zeitaltern und der Herrschaft der Unmenschen zu &uuml;berlassen.&ldquo;<br><br>
+
+Dieses Buch pr&auml;sentiert die Hauptaussagen von Ayn Rands Philosophie des Objektivismus &bdquo;f&uuml;r alle, die eine integrierte Weltanschauung suchen&ldquo;. Der Titelessay analysiert die westliche Kultur, diskutiert die Ursachen ihres Verlaufs, ihres Niedergangs, ihres gegenw&auml;rtigen Bankrotts und zeigt den Weg zu einer intellektuellen Renaissance auf.<br>
+</td></tr>
+
+<tr><td valign="top"><a rel="nofollow" href="http://www.amazon.de/gp/product/3902639377/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639377&linkCode=as2&tag=scholarium-21"><img border="0" src="http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=3902639377&Format=_SL250_&ID=AsinImage&MarketPlace=DE&ServiceVersion=20070822&WS=1&tag=scholarium-21" ></a><img src="http://ir-de.amazon-adsystem.com/e/ir?t=scholarium-21&l=as2&o=3&a=3902639377" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+</td>
+<td valign="top"><a href="http://www.amazon.de/gp/product/3902639377/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=3902639377&linkCode=as2&tag=scholarium-21"><b>Ludwig von Mises: Die Letztbegr&uuml;ndung der &Ouml;konomik: Ein methodologischer Essay</b></a> <br><br>Das letzte Buch des gro&szlig;en &ouml;sterreichischen &Ouml;konomen Ludwig von Mises ist ein w&uuml;rdiger Abschluss seines Lebenswerks und zu Unrecht bislang eher in Vergessenheit geraten. Erstmals liegt es nun in deutscher Sprache vor, da Mises nach der Flucht aus Europa in die USA fast nur noch auf Englisch schrieb.
+Es ist eines seiner leidenschaftlichsten B&uuml;cher: Mises zeigt auf, welche verh&auml;ngnisvollen Folgen erkenntnistheoretische Irrt&uuml;mer haben k&ouml;nnen. Wortgewaltig schreibt er gegen den Positivismus an, der zu einer Hybris von Technokraten f&uuml;hrte. Die &Ouml;konomik wurde so zu einer Kontroll- und Planungswissenschaft.<br><br>
+
+Mises begr&uuml;ndet als Essenz seines Lebenswerkes noch einmal in aller Klarheit die Alternative einer realistischen &Ouml;konomik als Wissenschaft vom menschlichen Handeln, abseits ideologischer Spekulation und pseudowissenschaftlicher Allmachtsanspr&uuml;che.
+Diese b&uuml;ndige, aber tiefgehende Darstellung der Irrwege moderner Wirtschaftswissenschaft und einer alternativen Perspektive, die dem Menschen in seiner Komplexit&auml;t und W&uuml;rde wirklich gerecht wird, k&ouml;nnte der Keim eines dringend n&ouml;tigen &ouml;konomischen Umdenkens sein. </td></tr></table>
+     
+	</div></body>
 </html>   
 
