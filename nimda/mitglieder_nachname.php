b9907ee21fd6feac7275c91eca3f56ca0ddb720c
WerteGK1 <gk@wertewirtschaft.org>
2016-03-01 10:56:31 +0100

    nimda mitglieder

--- nimda/mitglieder_nachname.php
+++ nimda/mitglieder_nachname.php
@@ -11,7 +11,7 @@
 <html lang="de">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-		<title>Mitgliederliste</title>
+		<title>Mitgliederliste (nach Nachname sortiert)</title>
 		
 		<style>
 		    @font-face {
