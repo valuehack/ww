ff2b5999d15ec1dc82f85bc4a86eae8efeb68cc1
Rahim <rt@wertewirtschaft.org>
2016-03-23 17:06:19 +0100

    Transaction error

--- medien/index.php
+++ medien/index.php
@@ -43,7 +43,7 @@ $login = new Login();
 //TODO Login and Email don't work together because of header send issues. It might be better to generate the email object wihtin he other functions to avoid this
 $email = new Email();
 $registration = new Registration();
-$transaction = new Transaction();
+// $transaction = new Transaction();
 
 
 // Mitgliedschaft 
