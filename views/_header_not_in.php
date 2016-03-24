73910bad5cbe4ee818ab9f025ece8298841acfd0
Rahim <rt@wertewirtschaft.org>
2016-03-24 19:21:50 +0100

    header_not_in changed

--- views/_header_not_in.php
+++ views/_header_not_in.php
@@ -157,18 +157,10 @@ mysql_query("SET time_zone = 'Europe/Vienna'");
                   <ul id="nav">
                       <li id="navelm"><a class="navelm" href="<?php BASE_URL ?>/fragen.php">H&auml;ufige Fragen</a></li>
              <li id="navelm"><a class="navelm" href="/scholien/">Scholien</a></li>
-                    <li id="navelm"><a class="navelm" id="drop1" data-toggle="dropdown" href="/veranstaltungen/" data-target="#" role="button" aria-haspopup="true" aria-expanded="false">Veranstaltungen</a>
-                    	<div class="subnav dropdown-menu" aria-labelledby="drop1">
-                    	<ul>
-                    		<li class="subnav_head"><a class="subnav_head" href="/veranstaltungen/">Veranstaltungen</a></li>
-                    		<li><a href="/veranstaltungen/">Alle</a></li>
-                    		<li><a href="/salon/">Salons</a></li>
-                    		<li><a href="/seminare/">Seminare</a></li>
-                    	</ul>
-                    	</div>
-                    </li>
+                    <li id="navelm"><a class="navelm" href="/salon/">Salon</a></li>
+                    <li id="navelm"><a class="navelm" href="/seminare/">Seminare</a></li>
     					<li id="navelm"><a class="navelm" href="/studium/">Studium</a></li>
-                      <li id="navelm"><a class="navelm" href="mailto:&#105;nf&#111;&#064;&#115;&#99;ho&#108;&#97;ri&#117;&#109;.&#97;&#116;">Kontakt</a></li>
+    					<li id="navelm"><a class="navelm" href="/buerger.php">Unterst&uuml;tzen</a></li>
                  </ul>
                 </div>
            </div>
