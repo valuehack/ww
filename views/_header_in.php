e46df1f2f77d1f4d35a195cb11e93b0e60563dd8
Rahim <rt@wertewirtschaft.org>
2016-03-30 14:38:29 +0200

    Bibliothek anzeigen

--- views/_header_in.php
+++ views/_header_in.php
@@ -253,7 +253,7 @@ while ($entry = mysql_fetch_array($result))
                     		<li><a href="/schriften/index.php?type=analysen">Analysen</a></li>
                     		<li><a href="/schriften/index.php?type=buecher">B&uuml;cher</a></li>
                     		<li><a href="/schriften/antiquariat.php">Antiquariat</a></li>
-                    		<li><a href="/schriften/bibliothek/">Bibliothek</a></li>
+                    		<li><a href="/bibliothek/">Bibliothek</a></li>
                     	</ul>
                     	</div>
                     </li>
