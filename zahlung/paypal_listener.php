2bda24a98ea82945009478b63eb848de469668bb
daneas <dzainius@gmail.com>
2016-04-03 16:13:30 +0200

    listener fix

--- zahlung/paypal_listener.php
+++ zahlung/paypal_listener.php
@@ -1,54 +1,69 @@
-		<?php 
+<?php 
+
+ini_set("log_errors" , "1");
+ini_set("error_log" , "php_listener_errors");
+ini_set("display_errors" , "0");
 
 	#response from paypal
-	$ipn_post_data = $_POST;
+$ipn_post_data = $_POST;
+
+error_log( "POST FROM PAYPAL. TXN_ID: ".$ipn_post_data['txn_id'] ." WRT_TXN_ID: ".$ipn_post_data['custom']);
+
+// define("LOG_FILE", "./listener.log");
+// error_log(date('[Y-m-d H:i e] '). "$ipn_post_data[test]". PHP_EOL, 3, LOG_FILE);
+
+require_once('../config/config.php');
+require_once('../classes/Registration.php');
+
+$registration = new Registration();
+
+try 
+{
+	$db_connection = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME . ';charset=utf8', DB_USER, DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+} 
+catch (PDOException $e) 
+{
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
+}
 
 
-	// define("LOG_FILE", "./listener.log");
-	// error_log(date('[Y-m-d H:i e] '). "$ipn_post_data[test]". PHP_EOL, 3, LOG_FILE);
+#writes serialized paypal responce to db  
+#TESTING ONLY
+$ipn_post_data_serialized = serialize($_POST);
 
-	require_once('../config/config.php');
-	require_once('../classes/Registration.php');
+$txn_id_test_query = $db_connection->prepare(
+"UPDATE mitgliederExt   
+    SET Notiz = :txn_id
+  WHERE user_email = :user_email"
+);
 
-	$registration = new Registration();
-
-	try 
-	{
-    	$db_connection = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME . ';charset=utf8', DB_USER, DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	} 
-	catch (PDOException $e) 
-	{
-	    echo 'Connection failed: ' . $e->getMessage();
-        exit;
-	}
+$txn_id_test_query->bindValue(':txn_id', $ipn_post_data_serialized, PDO::PARAM_STR);
+$txn_id_test_query->bindValue(':user_email', 'dzainius@gmail.com', PDO::PARAM_STR);
+$txn_id_test_query->execute();
 
 
-	#writes serialized paypal responce to db  
-	#TESTING ONLY
-	$ipn_post_data_serialized = serialize($_POST);
+#get data from db transactional db 
+$paypal_data_query = $db_connection->prepare(
+"SELECT * FROM paypal_data_storage 
+WHERE wrt_txn_id = :wrt_txn_id
+");
 
-    $txn_id_test_query = $db_connection->prepare(
-    "UPDATE mitgliederExt   
-        SET Notiz = :txn_id
-      WHERE user_email = :user_email"
-    );
+$paypal_data_query->bindValue(':wrt_txn_id', $ipn_post_data['custom'], PDO::PARAM_STR);
+// $paypal_data_query->bindValue(':wrt_txn_id', "0LbHNzBN6j24", PDO::PARAM_STR);
+$paypal_data_query->execute();
 
-    $txn_id_test_query->bindValue(':txn_id', $ipn_post_data_serialized, PDO::PARAM_STR);
-    $txn_id_test_query->bindValue(':user_email', 'dzainius@gmail.com', PDO::PARAM_STR);
-    $txn_id_test_query->execute();
+#wrt_txn_id was not found in the db.
+#
+if ($paypal_data_query->rowCount() == 0) 
+{
+	error_log( "No entry in db for:  ".$ipn_post_data['custom']);
 
-
-	#get data from db transactional db 
-	$paypal_data_query = $db_connection->prepare(
-	"SELECT * FROM paypal_data_storage 
-	WHERE wrt_txn_id = :wrt_txn_id
-	");
-
-	$paypal_data_query->bindValue(':wrt_txn_id', $ipn_post_data[custom], PDO::PARAM_STR);
-	// $paypal_data_query->bindValue(':wrt_txn_id', "0LbHNzBN6j24", PDO::PARAM_STR);
-	$paypal_data_query->execute();
-
+}
+else
+{
 	$result_row = $paypal_data_query->fetchObject();
+
 	$txn_data = unserialize($result_row->data);
 
 	$profile = $txn_data['profile'];
@@ -57,3 +72,6 @@
 	#register user
 	$registration->addNewUser($profile, $product);
 	$registration->addPersonalDataGeneric($profile);
+}
+
+
