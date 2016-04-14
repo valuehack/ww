4527293a6e45414d0c64340920d2ddeb6f1d8895
Rahim <rt@wertewirtschaft.org>
2016-04-14 17:49:46 +0200

    Format Unterstützen

--- views/_header_not_in.php
+++ views/_header_not_in.php
@@ -101,7 +101,7 @@ mysql_query("SET time_zone = 'Europe/Vienna'");
               }
               ?>
                     <div class="anmelden"><a href="http://en.scholarium.at/">English</a></div>
-                   <div class="anmelden"><a href="http://scholarium.at/spende/">Unterst&uuml;tzen</a></div> 
+                   <div class="anmelden"><a href="http://scholarium.at/spende/" class="login_button">Unterst&uuml;tzen</a></div> 
                   <div class="anmelden"><button class="login_button" type="button" data-toggle="modal" data-target="#signup" value="Anmelden">Eintragen</button></div>
                   <div class="anmelden"><button class="login_button" type="button" data-toggle="modal" data-target="#login" value="Anmelden">Anmelden</button></div>
 
