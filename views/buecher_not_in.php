c3f60d1115ff70186c393f5a340238b5fedd9c0d
WerteGK1 <gk@wertewirtschaft.org>
2016-05-18 16:08:07 +0200

    buecher + links

--- views/buecher_not_in.php
+++ views/buecher_not_in.php
@@ -15,7 +15,7 @@ if(isset($_GET['q']))
   $id = $_GET['q'];
 
   //Termindetails
-  $sql="SELECT * from produkte WHERE (type LIKE 'antiquariat' OR type LIKE 'buch') AND status = 1 AND (spots > spots_sold) order by title asc";
+  $sql = "SELECT * from produkte WHERE (type LIKE 'antiquariat' OR type LIKE 'buch' OR type LIKE 'analyse') AND status = 1 AND id='$id' order by title asc";
   $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
   $entry3 = mysql_fetch_array($result);
   
@@ -25,18 +25,24 @@ if(isset($_GET['q']))
 	if (@getimagesize($img)) {
 	    $img_url = $img;
 	} else {
-	    $img_url = "http://scholarium.at/schriften/default.jpg";
+	    $img_url = "";
 	}
 ?>  	
 	<div class="medien_head">
   		<h1><?echo $entry3[title]?></h1>
-  		<div class="schriften_img">
-			<img src="<?echo $img;?>" alt="<?echo $id;?>">
-		</div>
+  		<?php
+  			if ($img_url != "") {
+  			?>	
+  			<div class="schriften_img">
+				<img src="<?=$img_url?>" alt="<?=$id?>">
+			</div>
+			<?php
+			}
+			?>
 		<div class="schriften_bestellen">
 			<span class="schriften_type"><? echo ucfirst($entry3[type]);?></span>
 			 <!-- Button trigger modal -->
-			<input type="button" class="inputbutton" value="Bestellen und Herunterladen" data-toggle="modal" data-target="#myModal">
+			<input type="button" class="inputbutton" value="Bestellen" data-toggle="modal" data-target="#myModal">
 		</div>
 	</div>
 	<div class="medien_seperator">
@@ -47,7 +53,7 @@ if(isset($_GET['q']))
   if ($entry3[text]) echo $entry3[text];
   if ($entry3[text2]) echo $entry3[text2];
 ?>
-  		<div class="medien_anmeldung"><a href="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">zur&uuml;ck zu den Schriften</a></div>
+  		<div class="medien_anmeldung"><a href="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">zur&uuml;ck zu den B&uuml;chern</a></div>
   </div>
 <?php
 }
@@ -81,10 +87,10 @@ else {
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h2 class="modal-title" id="myModalLabel">Bestellen und Herunterladen</h2>
+        <h2 class="modal-title" id="myModalLabel">Bestellen</h2>
       </div>
       <div class="modal-body">
-  			<p>Wir freuen uns, dass Sie Interesse an unseren Schriften haben. Bitte tragen Sie hier Ihre E-Mail-Adresse ein, um mehr &uuml;ber die M&ouml;glichkeiten der Bestellung oder des Herunterladens digitaler Dateien zu erfahren (diese k&ouml;nnen wir leider nicht offen zug&auml;nglich machen):</p>
+  			<p>Wir freuen uns, dass Sie Interesse an unseren B&uuml;chern haben. Bitte tragen Sie hier Ihre E-Mail-Adresse ein, um mehr &uuml;ber die M&ouml;glichkeiten der Bestellung oder des Herunterladens digitaler Dateien zu erfahren (diese k&ouml;nnen wir leider nicht offen zug&auml;nglich machen):</p>
         <div class="subscribe">
           <form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" name="registerform">
           	<input class="inputfield" type="email" placeholder=" E-Mail-Adresse" name="user_email" required>
