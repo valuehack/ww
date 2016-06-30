ed086a9f44cd10e3be434b412dab326b4c2c8cfd
bartholos <um@wertewirtschaft.org>
2016-06-30 17:44:33 -0300

    misesatTowardsMaterialDesign

--- misesat/denker/index.php
+++ misesat/denker/index.php
@@ -97,7 +97,7 @@ else {
   	
 			<div class="container index-link"><p><a href="../index.php">Wiener Schule</a> / Denker</p></div>
       		<div class="container">
-      			<h1>Autoren</h1>
+      			<h1>Denker</h1>
       			<p>Hier finden Sie eine umfassende &Uuml;bersicht der in der Denktradition der klassischen Wiener/ &Ouml;sterreichischen Schulen stehenden Denker.</p>
       		</div>
       
