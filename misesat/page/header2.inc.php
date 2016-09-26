80ab4c0d87d6de6b688d6ca2a24db7a24e59713f
MerlinB <merlin.buczek@gmail.com>
2016-09-26 11:33:42 +0200

    ...

--- misesat/page/header2.inc.php
+++ misesat/page/header2.inc.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html >
-<html lang="de">	
+<html lang="de">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     	<meta name="description" content="Verlag und Kompendium zur &Ouml;stereichischen Schule der &Ouml;konomik">
     	<link rel="shortcut icon" href="..\favicon.ico">
         <link rel="icon" type="image/png" href="..\favicon.png" sizes="32x32">
- 
+
         <link rel="apple-touch-icon" sizes="180x180" href="..\apple-touch-icon.png">
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="msapplication-TileImage" content="..\mstile-144x144.png">
-    	
+
     	<meta name="viewport" content="width=device-width, initial-scale=1">
     	<!--[if IE]>
       		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
     	<![endif]-->
-    	
+
     	<link rel="stylesheet" href="../style/normalize.css">
     	<link rel="stylesheet" href="../style/style.css">
     	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300|EB+Garamond' rel='stylesheet' type='text/css'>
-                     
+
     	<title><?=$title?> | Mises Austria</title>
-        
+
 	</head>
 	<body>
 		<!-- Begin Header-->
 		<header id="header">
   			<div class="logo">
-  				<a href="../index.php"><img src="../style/ma_logo_text.svg" alt=""></a>
+  				<a href="../index.php"><img src="../style/ma_logo_text_neu.svg" alt=""></a>
 			</div>
 			<div class="nav-trigger">
 				<div class="nav-trigger__icon">
