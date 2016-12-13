681591e1450cf28c067eaf340fa8fb7e16a3b42f
MerlinB <merlin@Rahims-iMac-2.local>
2016-12-13 16:12:08 +0100

    Fotos

--- views/jubilaeum_in.php
+++ views/jubilaeum_in.php
@@ -22,25 +22,10 @@ include "../views/_header_not_in_jubilaeum.php";
 
       while ($i<=5) {
         //$img = "../bla/bla"+$i+".jpg";
-        echo "<figure><img src='https://googledrive.com/host/0B4pA_9bw5MghQVBEN29WaTQzVU0/Benjamin_Zika_20161203_Scholarium_0".$i."'.jpg' /></figure>";
+        echo "<figure><img src='../jubilaeum/Fotos/Benjamin_Zika_20161203_Scholarium_0".$i."'.jpg' /></figure>";
         $i++;
       }
     ?>
-
-
-
-      <figure>
-      <img src="../style/gfx/twitter.png" width="100%" />
-    </figure>
-    <figure>
-      <img src="../style/gfx/facebook.png" width="100%" /><figcaption>"Facebook" by <a href="https://www.google.com/">Trey Ratcliff</a>.</figcaption>
-    </figure>
-    <figure>
-      <img src="../style/gfx/xing.png" width="100%" /><figcaption>"Xing" by <a href="https://www.google.com/">Dave Soldano</a>.</figcaption>
-    </figure>
-
-      <!-- more figures here as needed -->
-
     </div>
     <script src="../js/slideshow.js"></script>
     <script>
