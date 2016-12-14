d2ccc769ff5330106b7c97c118199513e4cfffea
MerlinB <merlin@Rahims-iMac-2.local>
2016-12-14 14:58:54 +0100

    Jubilaeum

--- views/jubilaeum_not_in.php
+++ views/jubilaeum_not_in.php
@@ -12,7 +12,8 @@ include "../views/_header_not_in_jubilaeum.php";
   <p>Vielen Dank für 10 Jahre Unterstützung!</p>
   </div>
   <div class="jubilaeum_body">
-    <p>Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. Vielen Dank. </p>
+    <p>Das scholarium möchte sich an dieser Stelle bei allen Teilnehmern herzlichst Bedanken. Ohne Ihre treue Unterstützung hätte diese Jubiläumskonferenz nicht durchgeführt werden können. Bitte halten Sie uns weiterhin die Treue und lassen Sie uns im Jahr 2026 wieder ein gemeinsames Jubiläum feiern.</p>
+    <p>Für eine Auswahl an Fotos melden Sie sich bitte an.</p>
     <div class="jubilaeum_body2"><p class="anmelden"><button class="inputbutton" type="button" data-toggle="modal" data-target="#login" value="Anmelden">Bitte Anmelden</button></p></div>
   </div>
 
