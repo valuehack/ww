31fcf774ff2db67e3a5f1ddfa48c960f5b8687f1
geraldk1 <gk@scholarium.at>
2017-05-26 14:56:45 +0200

    Orientierungscoaching

--- views/programme_not_in.php
+++ views/programme_not_in.php
@@ -53,7 +53,7 @@ else {
      First get total number of rows in data table. 
      If you have a WHERE clause in your query, make sure you mirror it here.
   */
-  $query = "SELECT COUNT(*) as num FROM $tbl_name WHERE `type` LIKE 'programm'AND status > 0";
+  $query = "SELECT COUNT(*) as num FROM $tbl_name WHERE `type` LIKE 'programm'AND status = 1";
   $total_pages = mysql_fetch_array(mysql_query($query));
   $total_pages = $total_pages[num];
   
@@ -67,7 +67,7 @@ else {
     $start = 0;               //if no page var is given, set start to 0
   
   /* Get data. */
-  $sql = "SELECT * from produkte WHERE `type` LIKE 'programm' AND status > 0 order by n desc LIMIT $start, $limit";
+  $sql = "SELECT * from produkte WHERE `type` LIKE 'programm' AND status = 1 order by n desc LIMIT $start, $limit";
   
   $result = mysql_query($sql) or die("Failed Query of " . $sql. " - ". mysql_error());
   
