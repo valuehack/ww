b9eb250aa3ed326b14c6f4c3782aefe344ed43d3
gerald <gk@scholarium.at>
2017-06-23 10:58:04 +0200

    index redirect test

--- en/test1_index.php
+++ en/test1_index.php
@@ -154,47 +154,6 @@ mysql_query("SET time_zone = 'Europe/Vienna'");
 //if-condition to distinguish different headers (in vs. not_in); copied from _header-files in views-folder
 // here is the beginning of header_in
 
-$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-
-function ip_visitor_country()
-{
-
-    $client  = @$_SERVER['HTTP_CLIENT_IP'];
-    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-    $remote  = $_SERVER['REMOTE_ADDR'];
-    $country  = "Unknown";
-
-    if(filter_var($client, FILTER_VALIDATE_IP))
-    {
-        $ip = $client;
-    }
-    elseif(filter_var($forward, FILTER_VALIDATE_IP))
-    {
-        $ip = $forward;
-    }
-    else
-    {
-        $ip = $remote;
-    }
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://www.geoplugin.net/json.gp?ip=".$ip);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    $ip_data_in = curl_exec($ch); // string
-    curl_close($ch);
-
-    $ip_data = json_decode($ip_data_in,true);
-    $ip_data = str_replace('&quot;', '"', $ip_data); // for PHP 5.2 see stackoverflow.com/questions/3110487/
-
-    if($ip_data && $ip_data['geoplugin_countryName'] != null) {
-        $country = $ip_data['geoplugin_countryName'];
-    }
-
-    return $country;
-}
-echo $lang;
-echo ip_visitor_country();
-
 if ($login->isUserLoggedIn() == true) 
 {
 	$user_id = $_SESSION['user_id'];
--- test1_index.php
+++ test1_index.php
@@ -118,8 +118,6 @@ $email = new Email();
 $registration = new Registration();
 
 // ... ask if we are logged in here:
-echo $lang;
-echo ip_visitor_country();
 if ($login->isUserLoggedIn() == true) 
 {
 	include("views/index_in.php");
