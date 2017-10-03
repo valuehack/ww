422a92b662c81d7a02af2f39f06bdc4c79929d48
Merlin Buczek <merlin.buczek@gmail.com>
2017-10-03 18:42:28 +0200

    Überarbeitung

---
+++ spende/baustelle.php
@@ -0,0 +1,4 @@
+<div class="content">
+<p>Unser Zahlungssystem wird momentan überarbeitet. Bitte wenden Sie sich an
+<a href="mailto:&#105;nf&#111;&#064;&#115;&#99;ho&#108;&#97;ri&#117;&#109;.&#97;&#116;">&#105;nf&#111;&#064;&#115;&#99;ho&#108;&#97;ri&#117;&#109;.&#97;&#116;</a></p>
+</div class="content">
